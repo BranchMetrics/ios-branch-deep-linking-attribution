89d09436e73c707f6bb05f877c3aeb7d6f004fbe
qgong <qinwei@branchmetrics.io>
2014-11-21 14:33:05 -0800

    Updated version to 0.3.0

--- Branch-SDK/Branch-SDK/BNCConfig.h
+++ Branch-SDK/Branch-SDK/BNCConfig.h
@@ -9,7 +9,7 @@
 #ifndef Branch_SDK_Config_h
 #define Branch_SDK_Config_h
 
-#define SDK_VERSION             @"0.2.9"
+#define SDK_VERSION             @"0.3.0"
 
 #define PROD_ENV
 //#define STAGE_ENV
