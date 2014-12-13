0f048884831c0d93c7ec9891b851b94aafb07298
Alex Austin <alexjaustin@gmail.com>
2014-12-12 19:22:22 -0800

    Updated with else case for non init

--- Branch-SDK/Branch-SDK/BNCConfig.h
+++ Branch-SDK/Branch-SDK/BNCConfig.h
@@ -9,7 +9,7 @@
 #ifndef Branch_SDK_Config_h
 #define Branch_SDK_Config_h
 
-#define SDK_VERSION             @"0.3.6"
+#define SDK_VERSION             @"0.3.7"
 
 #define PROD_ENV
 //#define STAGE_ENV
