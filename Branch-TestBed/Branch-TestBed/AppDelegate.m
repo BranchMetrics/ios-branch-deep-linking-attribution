9c78695556406607e963c17994122acc3ca8511c
Alex Austin <alexjaustin@gmail.com>
2015-01-17 21:08:20 -0800

    test app

--- Branch-TestBed/Branch-TestBed/AppDelegate.m
+++ Branch-TestBed/Branch-TestBed/AppDelegate.m
@@ -12,7 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    Branch *branch = [Branch getInstance:@"5680621892404085"];
+    Branch *branch = [Branch getInstance:@"5668720416392049"];
     [Branch setDebug];
     [branch initSessionWithLaunchOptions:launchOptions isReferrable:YES andRegisterDeepLinkHandler:^(NSDictionary *params, NSError *error) {
         if (!error) {
