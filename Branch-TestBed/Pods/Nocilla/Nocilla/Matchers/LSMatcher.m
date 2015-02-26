a8399b9f16e2a1087cc8b3e64f1e183c5dcb87da
Graham Mueller <graham.mueller315@gmail.com>
2015-02-25 21:37:46 -0600

    Ignoring Pods directory.

--- Branch-TestBed/Pods/Nocilla/Nocilla/Matchers/LSMatcher.m
+++
@@ -1,9 +0,0 @@
-#import "LSMatcher.h"
-
-@implementation LSMatcher
-
-- (BOOL)matches:(NSString *)string {
-    @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"[LSMatcher matches:] is an abstract method" userInfo:nil];
-}
-
-@end
--- Branch-TestBed/Pods/Target Support Files/Pods-Branch-SDK test-Nocilla/Pods-Branch-SDK test-Nocilla-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Branch_SDK_test_Nocilla : NSObject
-@end
-@implementation PodsDummy_Pods_Branch_SDK_test_Nocilla
-@end
