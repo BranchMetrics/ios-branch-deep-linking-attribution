6e2f5267aa078ccc0d9d7948878b470c302d0fb4
Graham Mueller <graham.mueller315@gmail.com>
2015-04-06 21:40:58 -0500

    Temporarily removing deprecation warning. Turns out the backend update for this is not live yet. Updating ServerInterface to only send values if present. NO_STRING_VALUE will not make it to the server. We may need to do some additional work to prevent any calls from getting as far as these methods.

--- Branch-SDK/Branch-SDK/BNCPreferenceHelper.m
+++ Branch-SDK/Branch-SDK/BNCPreferenceHelper.m
@@ -198,7 +198,8 @@ static NSString *Branch_Key = nil;
 }
 
 + (void)setAppKey:(NSString *)appKey {
-    NSLog(@"Usage of App Key is deprecated, please move toward using a Branch key");
+    //TODO re-deprecate
+//    NSLog(@"Usage of App Key is deprecated, please move toward using a Branch key");
     [BNCPreferenceHelper writeObjectToDefaults:KEY_APP_KEY value:appKey];
 }
 
