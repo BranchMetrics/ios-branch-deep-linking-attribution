28e744cf2cbf00c2c2340ef4161e1ea8a9343910
Graham Mueller <graham.mueller315@gmail.com>
2015-04-09 12:03:23 -0500

    Updating PreferenceHelper to listen to the callback of setDebug to verify server accepted connection. Fixing an infinite loop issue trying to log in encoding utils. BNCPreferenceHelper log tries to send the message to server, which additionally runs through the encoding (loop). Merging PreferenceHelper and Debugger tests; removing debugger scheme. Removing Debugger and Stubless test artifacts.

--- Branch-TestBed/Pods/Target Support Files/Pods-Branch-SDK Stubless Tests-Nocilla/Pods-Branch-SDK Stubless Tests-Nocilla-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Branch_SDK_Stubless_Tests_Nocilla : NSObject
-@end
-@implementation PodsDummy_Pods_Branch_SDK_Stubless_Tests_Nocilla
-@end
