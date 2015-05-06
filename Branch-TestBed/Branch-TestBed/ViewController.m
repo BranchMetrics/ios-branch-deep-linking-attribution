53eea1834ff31ba8b62343bf0c94af23de72d27b
Graham Mueller <graham@branchmetrics.io>
2015-05-06 12:24:39 -0500

    Updating credit count after a successful redemption. Ideally an additional load wouldn't be necessary -- this is addressed in the rework branch.

--- Branch-TestBed/Branch-TestBed/ViewController.m
+++ Branch-TestBed/Branch-TestBed/ViewController.m
@@ -60,7 +60,11 @@
             NSLog(@"didn't redeem anything: %@", error);
         }
         else {
-            NSLog(@"redeemed 5 credits!");
+            NSLog(@"redeemed 5 credits! updating count now");
+            
+            [branch loadRewardsWithCallback:^(BOOL changed, NSError *error) {
+                [self.txtRewardCredits setText:[NSString stringWithFormat:@"%ld", (long)[branch getCredits]]];
+            }];
         }
     }];
 }
