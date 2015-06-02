a5bc42fa516c73169c194ecb795651946bef5a26
Graham Mueller <graham@branchmetrics.io>
2015-06-02 12:13:34 -0500

    Fix ACL for uploaded files. Zip entire directory (r). Keep symlinks in zip (y). Quiet zip (q). Remove zips after upload. Status messages.

--- scripts/upload_zips.sh
+++ scripts/upload_zips.sh
@@ -1,7 +1,16 @@
 #/bin/sh
 
-zip Branch-iOS-SDK.zip Branch-SDK/ Branch.framework/
-zip Branch-iOS-TestBed.zip Branch-SDK/ Branch-TestBed/ Branch.framework/
+echo 'Zipping Branch-iOS-SDK'
+zip -rqy Branch-iOS-SDK.zip Branch-SDK/ Branch.framework/
 
-aws s3 cp Branch-iOS-SDK.zip s3://branchhost/
-aws s3 cp Branch-iOS-TestBed.zip s3://branchhost/
+echo 'Zipping Branch-iOS-TestBed'
+zip -rqy Branch-iOS-TestBed.zip Branch-SDK/ Branch-TestBed/ Branch.framework/
+
+echo 'Uploading Branch-iOS-SDK'
+aws s3 cp --acl public-read Branch-iOS-SDK.zip s3://branchhost/
+
+echo 'Uploading Branch-iOS-TestBed'
+aws s3 cp --acl public-read Branch-iOS-TestBed.zip s3://branchhost/
+
+echo 'Removing zip files'
+rm Branch-iOS-SDK.zip Branch-iOS-TestBed.zip
