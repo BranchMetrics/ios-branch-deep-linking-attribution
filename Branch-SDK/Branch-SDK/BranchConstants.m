3b6df18e82dd3951d1eb61a532f73ca608b0d80d
Graham Mueller <graham@branchmetrics.io>
2015-06-10 14:02:58 -0500

    Creating tests around SetIdentity. Moving request and response constants back to their own class

---
+++ Branch-SDK/Branch-SDK/BranchConstants.m
@@ -0,0 +1,18 @@
+//
+//  BranchConstants.m
+//  Branch-TestBed
+//
+//  Created by Graham Mueller on 6/10/15.
+//  Copyright (c) 2015 Branch Metrics. All rights reserved.
+//
+
+#import "BranchConstants.h"
+
+NSString * const BRANCH_REQUEST_KEY_BRANCH_IDENTITY = @"identity_id";
+NSString * const BRANCH_REQUEST_KEY_DEVELOPER_IDENTITY = @"identity";
+NSString * const BRANCH_REQUEST_KEY_DEVICE_FINGERPRINT_ID = @"device_fingerprint_id";
+NSString * const BRANCH_REQUEST_KEY_SESSION_ID = @"session_id";
+
+NSString * const BRANCH_RESPONSE_KEY_BRANCH_IDENTITY = @"identity_id";
+NSString * const BRANCH_RESPONSE_KEY_USER_URL = @"link";
+NSString * const BRANCH_RESPONSE_KEY_INSTALL_PARAMS = @"referring_data";
