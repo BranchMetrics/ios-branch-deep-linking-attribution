ed52a8173ba7c6b03fde6b6e41308786c0b5214a
Graham Mueller <graham@branchmetrics.io>
2015-06-12 10:08:15 -0500

    Adding constants and tests around LoadActions request.

--- Branch-SDK/Branch-SDK/BranchConstants.m
+++ Branch-SDK/Branch-SDK/BranchConstants.m
@@ -19,3 +19,5 @@ NSString * const BRANCH_RESPONSE_KEY_BRANCH_IDENTITY = @"identity_id";
 NSString * const BRANCH_RESPONSE_KEY_SESSION_ID = @"session_id";
 NSString * const BRANCH_RESPONSE_KEY_USER_URL = @"link";
 NSString * const BRANCH_RESPONSE_KEY_INSTALL_PARAMS = @"referring_data";
+NSString * const BRANCH_RESPONSE_KEY_ACTION_COUNT_TOTAL = @"total";
+NSString * const BRANCH_RESPONSE_KEY_ACTION_COUNT_UNIQUE = @"unique";
