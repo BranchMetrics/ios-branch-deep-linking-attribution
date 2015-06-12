224dad787bb72a6c19e4961374c414e2e10e96f4
Graham Mueller <graham@branchmetrics.io>
2015-06-12 16:04:33 -0500

    Adding constants and tests around Get Referral Code. Defaulting preference values for tests.

--- Branch-SDK/Branch-SDK/BranchConstants.m
+++ Branch-SDK/Branch-SDK/BranchConstants.m
@@ -19,6 +19,12 @@ NSString * const BRANCH_REQUEST_KEY_AMOUNT = @"amount";
 NSString * const BRANCH_REQUEST_KEY_LENGTH = @"length";
 NSString * const BRANCH_REQUEST_KEY_DIRECTION = @"direction";
 NSString * const BRANCH_REQUEST_KEY_STARTING_TRANSACTION_ID = @"begin_after_id";
+NSString * const BRANCH_REQUEST_KEY_REFERRAL_CALCULATION_TYPE = @"calculation_type";
+NSString * const BRANCH_REQUEST_KEY_REFERRAL_REWARD_LOCATION = @"location";
+NSString * const BRANCH_REQUEST_KEY_REFERRAL_TYPE = @"type";
+NSString * const BRANCH_REQUEST_KEY_REFERRAL_CREATION_SOURCE = @"creation_source";
+NSString * const BRANCH_REQUEST_KEY_REFERRAL_PREFIX = @"prefix";
+NSString * const BRANCH_REQUEST_KEY_REFERRAL_EXPIRATION = @"expiration";
 
 NSString * const BRANCH_RESPONSE_KEY_BRANCH_IDENTITY = @"identity_id";
 NSString * const BRANCH_RESPONSE_KEY_SESSION_ID = @"session_id";
@@ -28,3 +34,4 @@ NSString * const BRANCH_RESPONSE_KEY_ACTION_COUNT_TOTAL = @"total";
 NSString * const BRANCH_RESPONSE_KEY_ACTION_COUNT_UNIQUE = @"unique";
 NSString * const BRANCH_RESPONSE_KEY_REFERRER = @"referrer";
 NSString * const BRANCH_RESPONSE_KEY_REFERREE = @"referree";
+NSString * const BRANCH_RESPONSE_KEY_REFERRAL_CODE = @"referral_code";
