f10073fbb3b1e2ae62c63bd5793905203c0952a1
Graham Mueller <graham@branchmetrics.io>
2015-06-19 14:51:02 -0500

    Fix all the promo code tests.

--- Branch-TestBed/Branch-SDK-Tests/BranchGetPromoCodeRequestTests.m
+++ Branch-TestBed/Branch-SDK-Tests/BranchGetPromoCodeRequestTests.m
@@ -1,5 +1,5 @@
 //
-//  BranchGetReferralCodeRequestTests.m
+//  BranchGetPromoCodeRequestTests.m
 //  Branch-TestBed
 //
 //  Created by Graham Mueller on 6/12/15.
@@ -7,17 +7,17 @@
 //
 
 #import "BranchTest.h"
-#import "BranchGetReferralCodeRequest.h"
+#import "BranchGetPromoCodeRequest.h"
 #import "BNCPreferenceHelper.h"
 #import "BranchConstants.h"
 #import "BNCError.h"
 #import <OCMock/OCMock.h>
 
-@interface BranchGetReferralCodeRequestTests : BranchTest
+@interface BranchGetPromoCodeRequestTests : BranchTest
 
 @end
 
-@implementation BranchGetReferralCodeRequestTests
+@implementation BranchGetPromoCodeRequestTests
 
 - (void)testRequestBodyWithAllItemsSpecified {
     NSString * const BUCKET = @"foo_bucket";
@@ -31,15 +31,15 @@
         BRANCH_REQUEST_KEY_BRANCH_IDENTITY: [BNCPreferenceHelper getIdentityID],
         BRANCH_REQUEST_KEY_DEVICE_FINGERPRINT_ID: [BNCPreferenceHelper getDeviceFingerprintID],
         BRANCH_REQUEST_KEY_SESSION_ID: [BNCPreferenceHelper getSessionID],
-        BRANCH_REQUEST_KEY_REFERRAL_CALCULATION_TYPE: @(BranchUniqueRewards),
-        BRANCH_REQUEST_KEY_REFERRAL_REWARD_LOCATION: @(BranchReferringUser),
+        BRANCH_REQUEST_KEY_REFERRAL_USAGE_TYPE: @(BranchPromoCodeUsageTypeOncePerUser),
+        BRANCH_REQUEST_KEY_REFERRAL_REWARD_LOCATION: @(BranchPromoCodeRewardReferringUser),
         BRANCH_REQUEST_KEY_REFERRAL_TYPE: @"credit",
         BRANCH_REQUEST_KEY_REFERRAL_CREATION_SOURCE: @2,
         BRANCH_REQUEST_KEY_REFERRAL_PREFIX: PREFIX,
         BRANCH_REQUEST_KEY_REFERRAL_EXPIRATION: EXPIRATION
     };
     
-    BranchGetReferralCodeRequest *request = [[BranchGetReferralCodeRequest alloc] initWithCalcType:BranchUniqueRewards location:BranchReferringUser amount:AMOUNT bucket:BUCKET prefix:PREFIX expiration:EXPIRATION callback:NULL];
+    BranchGetPromoCodeRequest *request = [[BranchGetPromoCodeRequest alloc] initWithUsageType:BranchPromoCodeUsageTypeOncePerUser rewardLocation:BranchPromoCodeRewardReferringUser amount:AMOUNT bucket:BUCKET prefix:PREFIX expiration:EXPIRATION useOld:NO callback:NULL];
     id serverInterfaceMock = OCMClassMock([BNCServerInterface class]);
     [[serverInterfaceMock expect] postRequest:expectedParams url:[OCMArg any] key:[OCMArg any] callback:[OCMArg any]];
     
@@ -58,13 +58,13 @@
         BRANCH_REQUEST_KEY_BRANCH_IDENTITY: [BNCPreferenceHelper getIdentityID],
         BRANCH_REQUEST_KEY_DEVICE_FINGERPRINT_ID: [BNCPreferenceHelper getDeviceFingerprintID],
         BRANCH_REQUEST_KEY_SESSION_ID: [BNCPreferenceHelper getSessionID],
-        BRANCH_REQUEST_KEY_REFERRAL_CALCULATION_TYPE: @(BranchUniqueRewards),
-        BRANCH_REQUEST_KEY_REFERRAL_REWARD_LOCATION: @(BranchReferringUser),
+        BRANCH_REQUEST_KEY_REFERRAL_USAGE_TYPE: @(BranchPromoCodeUsageTypeOncePerUser),
+        BRANCH_REQUEST_KEY_REFERRAL_REWARD_LOCATION: @(BranchPromoCodeRewardReferringUser),
         BRANCH_REQUEST_KEY_REFERRAL_TYPE: @"credit",
         BRANCH_REQUEST_KEY_REFERRAL_CREATION_SOURCE: @2
     };
     
-    BranchGetReferralCodeRequest *request = [[BranchGetReferralCodeRequest alloc] initWithCalcType:BranchUniqueRewards location:BranchReferringUser amount:AMOUNT bucket:BUCKET prefix:nil expiration:nil callback:NULL];
+    BranchGetPromoCodeRequest *request = [[BranchGetPromoCodeRequest alloc] initWithUsageType:BranchPromoCodeUsageTypeOncePerUser rewardLocation:BranchPromoCodeRewardReferringUser amount:AMOUNT bucket:BUCKET prefix:nil expiration:nil useOld:NO callback:NULL];
     id serverInterfaceMock = OCMClassMock([BNCServerInterface class]);
     [[serverInterfaceMock expect] postRequest:expectedParams url:[OCMArg any] key:[OCMArg any] callback:[OCMArg any]];
     
@@ -73,13 +73,30 @@
     [serverInterfaceMock verify];
 }
 
-- (void)testBasicSuccess {
+- (void)testBasicSuccessWithPromoCode {
+    NSDictionary * const REFERRAL_RESPONSE_DATA = @{ BRANCH_RESPONSE_KEY_PROMO_CODE: @"foo" };
+    BNCServerResponse *response = [[BNCServerResponse alloc] init];
+    response.data = REFERRAL_RESPONSE_DATA;
+    
+    XCTestExpectation *requestExpecation = [self expectationWithDescription:@"Get Promo Code Request Expectation"];
+    BranchGetPromoCodeRequest *request = [[BranchGetPromoCodeRequest alloc] initWithUsageType:0 rewardLocation:0 amount:0 bucket:nil prefix:nil expiration:nil useOld:NO callback:^(NSDictionary *params, NSError *error) {
+        XCTAssertEqualObjects(params, REFERRAL_RESPONSE_DATA);
+        XCTAssertNil(error);
+        [self safelyFulfillExpectation:requestExpecation];
+    }];
+    
+    [request processResponse:response error:nil];
+    
+    [self awaitExpectations];
+}
+
+- (void)testBasicSuccessWithReferralCode {
     NSDictionary * const REFERRAL_RESPONSE_DATA = @{ BRANCH_RESPONSE_KEY_REFERRAL_CODE: @"foo" };
     BNCServerResponse *response = [[BNCServerResponse alloc] init];
     response.data = REFERRAL_RESPONSE_DATA;
     
-    XCTestExpectation *requestExpecation = [self expectationWithDescription:@"Get Referral Code Request Expectation"];
-    BranchGetReferralCodeRequest *request = [[BranchGetReferralCodeRequest alloc] initWithCalcType:0 location:0 amount:0 bucket:nil prefix:nil expiration:nil callback:^(NSDictionary *params, NSError *error) {
+    XCTestExpectation *requestExpecation = [self expectationWithDescription:@"Get Promo Code Request Expectation"];
+    BranchGetPromoCodeRequest *request = [[BranchGetPromoCodeRequest alloc] initWithUsageType:0 rewardLocation:0 amount:0 bucket:nil prefix:nil expiration:nil useOld:YES callback:^(NSDictionary *params, NSError *error) {
         XCTAssertEqualObjects(params, REFERRAL_RESPONSE_DATA);
         XCTAssertNil(error);
         [self safelyFulfillExpectation:requestExpecation];
@@ -95,8 +112,8 @@
     BNCServerResponse *response = [[BNCServerResponse alloc] init];
     response.data = RESPONSE_DATA;
     
-    XCTestExpectation *requestExpecation = [self expectationWithDescription:@"Get Referral Code Request Expectation"];
-    BranchGetReferralCodeRequest *request = [[BranchGetReferralCodeRequest alloc] initWithCalcType:0 location:0 amount:0 bucket:nil prefix:nil expiration:nil callback:^(NSDictionary *params, NSError *error) {
+    XCTestExpectation *requestExpecation = [self expectationWithDescription:@"Get Promo Code Request Expectation"];
+    BranchGetPromoCodeRequest *request = [[BranchGetPromoCodeRequest alloc] initWithUsageType:0 rewardLocation:0 amount:0 bucket:nil prefix:nil expiration:nil useOld:NO callback:^(NSDictionary *params, NSError *error) {
         XCTAssertEqualObjects(params, RESPONSE_DATA);
         XCTAssertEqualObjects(error.domain, BNCErrorDomain);
         XCTAssertEqual(error.code, BNCInvalidReferralCodeError);
@@ -111,8 +128,8 @@
 - (void)testBasicFailure {
     NSError * const REQUEST_ERROR = [NSError errorWithDomain:@"foo" code:1 userInfo:nil];
     
-    XCTestExpectation *requestExpecation = [self expectationWithDescription:@"Get Referral Code Request Expectation"];
-    BranchGetReferralCodeRequest *request = [[BranchGetReferralCodeRequest alloc] initWithCalcType:0 location:0 amount:0 bucket:nil prefix:nil expiration:nil callback:^(NSDictionary *params, NSError *error) {
+    XCTestExpectation *requestExpecation = [self expectationWithDescription:@"Get Promo Code Request Expectation"];
+    BranchGetPromoCodeRequest *request = [[BranchGetPromoCodeRequest alloc] initWithUsageType:0 rewardLocation:0 amount:0 bucket:nil prefix:nil expiration:nil useOld:NO callback:^(NSDictionary *params, NSError *error) {
         XCTAssertNil(params);
         XCTAssertEqualObjects(error, REQUEST_ERROR);
         [self safelyFulfillExpectation:requestExpecation];
