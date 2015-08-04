cabcd33956962f61919bcdfb0ddd9d49cc42c372
Graham Mueller <graham@branchmetrics.io>
2015-08-04 13:26:18 -0500

    Adding a marker interface for BNCDebugRequests so they aren't persisted or loaded from disk. Updating tests to reflect this expectation. Reverting Log Request NSCoding methods, since these shouldn't be persisted anymore.

---
+++ Branch-SDK/Branch-SDK/BNCDebugRequest.h
@@ -0,0 +1,13 @@
+//
+//  BNCDebugRequest.h
+//  Branch-TestBed
+//
+//  Created by Graham Mueller on 8/4/15.
+//  Copyright © 2015 Branch Metrics. All rights reserved.
+//
+
+#import <Branch/Branch.h>
+
+@interface BNCDebugRequest : BNCServerRequest
+
+@end
