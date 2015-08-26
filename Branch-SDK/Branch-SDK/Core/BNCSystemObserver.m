839337006bb237b2a1d4ff54de4f86f684b6b2c0
Derrick Staten <statenderrick@gmail.com>
2015-08-26 11:57:02 -0700

    don't crash on 1 or 2-char uri schemes

--- Branch-SDK/Branch-SDK/Core/BNCSystemObserver.m
+++ Branch-SDK/Branch-SDK/Core/BNCSystemObserver.m
@@ -19,7 +19,7 @@
 + (NSString *)getUniqueHardwareId:(BOOL *)isReal andIsDebug:(BOOL)debug {
     NSString *uid = nil;
     *isReal = YES;
-    
+
     Class ASIdentifierManagerClass = NSClassFromString(@"ASIdentifierManager");
     if (ASIdentifierManagerClass && !debug) {
         SEL sharedManagerSelector = NSSelectorFromString(@"sharedManager");
@@ -28,16 +28,16 @@
         NSUUID *uuid = ((NSUUID* (*)(id, SEL))[sharedManager methodForSelector:advertisingIdentifierSelector])(sharedManager, advertisingIdentifierSelector);
         uid = [uuid UUIDString];
     }
-    
+
     if (!uid && NSClassFromString(@"UIDevice")) {
         uid = [[UIDevice currentDevice].identifierForVendor UUIDString];
     }
-    
+
     if (!uid) {
         uid = [[NSUUID UUID] UUIDString];
         *isReal = NO;
     }
-    
+
     return uid;
 }
 
@@ -59,15 +59,17 @@
     for (NSDictionary *urlType in urlTypes) {
         NSArray *urlSchemes = [urlType objectForKey:@"CFBundleURLSchemes"];
         for (NSString *urlScheme in urlSchemes) {
-            NSString *firstTwoCharacters = [urlScheme substringWithRange:NSMakeRange(0, 2)];
-            NSString *firstThreeCharacters = [urlScheme substringWithRange:NSMakeRange(0, 3)];
-            BOOL isFBScheme = [firstTwoCharacters isEqualToString:@"fb"];
-            BOOL isDBScheme = [firstTwoCharacters isEqualToString:@"db"];
-            BOOL isPinScheme = [firstThreeCharacters isEqualToString:@"pin"];
-            
-            // Don't use the schemes set aside for other integrations.
-            if (!isFBScheme && !isDBScheme && !isPinScheme) {
-                return urlScheme;
+            if (urlScheme.length > 2) {
+                NSString *firstTwoCharacters = [urlScheme substringWithRange:NSMakeRange(0, 2)];
+                NSString *firstThreeCharacters = [urlScheme substringWithRange:NSMakeRange(0, 3)];
+                BOOL isFBScheme = [firstTwoCharacters isEqualToString:@"fb"];
+                BOOL isDBScheme = [firstTwoCharacters isEqualToString:@"db"];
+                BOOL isPinScheme = [firstThreeCharacters isEqualToString:@"pin"];
+
+                // Don't use the schemes set aside for other integrations.
+                if (!isFBScheme && !isDBScheme && !isPinScheme) {
+                    return urlScheme;
+                }
             }
         }
     }
@@ -119,12 +121,12 @@
 
 + (NSString *)getCarrier {
     NSString *carrierName = nil;
-    
+
     Class CTTelephonyNetworkInfoClass = NSClassFromString(@"CTTelephonyNetworkInfo");
     if (CTTelephonyNetworkInfoClass) {
         id networkInfo = [[CTTelephonyNetworkInfoClass alloc] init];
         SEL subscriberCellularProviderSelector = NSSelectorFromString(@"subscriberCellularProvider");
-        
+
         id carrier = ((id (*)(id, SEL))[networkInfo methodForSelector:subscriberCellularProviderSelector])(networkInfo, subscriberCellularProviderSelector);
         if (carrier) {
             SEL carrierNameSelector = NSSelectorFromString(@"carrierName");
@@ -142,7 +144,7 @@
 + (NSString *)getModel {
     struct utsname systemInfo;
     uname(&systemInfo);
-    
+
     return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
 }
 
@@ -165,17 +167,17 @@
     NSString *storedAppVersion = [BNCPreferenceHelper preferenceHelper].appVersion;
     NSString *currentAppVersion = [BNCSystemObserver getAppVersion];
     NSFileManager *manager = [NSFileManager defaultManager];
-    
+
     // for creation date
     NSURL *documentsDirRoot = [[manager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
     NSDictionary *documentsDirAttributes = [manager attributesOfItemAtPath:documentsDirRoot.path error:nil];
     NSDate *creationDate = [documentsDirAttributes fileCreationDate];
-    
+
     // for modification date
     NSString *bundleRoot = [[NSBundle mainBundle] bundlePath];
     NSDictionary *bundleAttributes = [manager attributesOfItemAtPath:bundleRoot error:nil];
     NSDate *modificationDate = [bundleAttributes fileModificationDate];
-    
+
     // No stored version
     if (!storedAppVersion) {
         // Modification and Creation date are more than 24 hours' worth of seconds different indicates
@@ -184,7 +186,7 @@
         if (ABS([modificationDate timeIntervalSinceDate:creationDate]) > 86400) {
             return @2;
         }
-        
+
         // If we don't have one of the previous dates, or they're less than 60 apart,
         // we understand this to be an install.
         return @0;
@@ -193,7 +195,7 @@
     else if (![storedAppVersion isEqualToString:currentAppVersion]) {
         return @2;
     }
-    
+
     // Otherwise, we have a stored version, and it is equal.
     // Not an update, not an install.
     return @1;
@@ -231,7 +233,7 @@
     NSMutableArray *appsPresent = [[NSMutableArray alloc] init];
     NSMutableArray *appsNotPresent = [[NSMutableArray alloc] init];
     NSDictionary *appsData = @{ @"canOpen": appsPresent, @"notOpen": appsNotPresent };
-    
+
     UIApplication *application = [UIApplication sharedApplication];
     for (NSString *app in apps) {
         NSString *uriScheme = app;
@@ -247,7 +249,7 @@
             [appsNotPresent addObject:app];
         }
     }
-    
+
     return appsData;
 }
 
