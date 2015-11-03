f6546e4194a2c1aa12a8a88a330fd443cca0239a
Dan Walkowski <dan.walkowski@gmail.com>
2015-11-03 09:03:38 -0800

    Rewrote ServerInterface tests to use OHHTTPStubs, and utilize NSURLSession instead of NSURLRequest.

--- Branch-TestBed/Branch-SDK-Tests/BNCServerInterfaceTests.m
+++ Branch-TestBed/Branch-SDK-Tests/BNCServerInterfaceTests.m
@@ -11,6 +11,9 @@
 #import "BNCPreferenceHelper.h"
 #import <OCMock/OCMock.h>
 #import <OHHTTPStubs/OHHTTPStubs.h>
+#import "OHHTTPStubsResponse+JSON.h"
+
+
 
 typedef void (^UrlConnectionCallback)(NSURLResponse *, NSData *, NSError *);
 
@@ -21,29 +24,20 @@ typedef void (^UrlConnectionCallback)(NSURLResponse *, NSData *, NSError *);
 @implementation BNCServerInterfaceTests
 
 #pragma mark - Tear Down
-- (void) tearDown {
-    [super tearDown];
-    [OHHTTPStubs removeAllStubs];
+- (void)tearDown
+{
+  [OHHTTPStubs removeAllStubs];
+  [super tearDown];
 }
 
 
 #pragma mark - Key tests
+//==================================================================================
+//TEST 01
+//This test checks to see that the branch key has been added to the GET request
 
 - (void)testParamAddForBranchKey {
     BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
-    // temporarily commented out due to iOS 9 testing only.
-    /*id urlConnectionMock = OCMClassMock([NSURLConnection class]);
-    
-    // Expect the query to contain branch key
-    [[urlConnectionMock expect] sendAsynchronousRequest:[OCMArg checkWithBlock:^BOOL(NSURLRequest *request) {
-        return [request.URL.query rangeOfString:@"branch_key=key_foo"].location != NSNotFound;
-    }] queue:[OCMArg any] completionHandler:[OCMArg any]];
-    
-    // Make the request
-    [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:NULL];
-
-    [urlConnectionMock verify];*/
-
     XCTestExpectation* expectation = [self expectationWithDescription:@"NSURLSessionDataTask completed"];
 
     [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
@@ -62,21 +56,12 @@ typedef void (^UrlConnectionCallback)(NSURLResponse *, NSData *, NSError *);
 
 }
 
+//==================================================================================
+//TEST 02
+//This test checks to see that the branch key has been added to the GET request
+
 - (void)testParamAddForAppKey {
     BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
-    // temporarily commented out due to iOS 9 testing only.
-    /*id urlConnectionMock = OCMClassMock([NSURLConnection class]);
-    
-    // Expect the query to contain app id
-    [[urlConnectionMock expect] sendAsynchronousRequest:[OCMArg checkWithBlock:^BOOL(NSURLRequest *request) {
-        return [request.URL.query rangeOfString:@"app_id=non_branch_key"].location != NSNotFound;
-    }] queue:[OCMArg any] completionHandler:[OCMArg any]];
-    
-    // Make the request
-    [serverInterface getRequest:nil url:@"http://foo" key:@"non_branch_key" callback:NULL];
-    
-    [urlConnectionMock verify];*/
-
     XCTestExpectation* expectation = [self expectationWithDescription:@"NSURLSessionDataTask completed"];
 
     [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
@@ -95,167 +80,270 @@ typedef void (^UrlConnectionCallback)(NSURLResponse *, NSData *, NSError *);
 }
 
 
+
+
 #pragma mark - Retry tests
+//==================================================================================
+//TEST 03
+//This test simulates a poor network, with three failed GET attempts and one final success,
+// for 4 connections.
 
 - (void)testGetRequestAsyncRetriesWhenAppropriate {
-    BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
-    serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
-    /*id urlConnectionMock = OCMClassMock([NSURLConnection class]);
+  
+  //Set up nsurlsession and data task, catching response
+  BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
+  serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
+  serverInterface.preferenceHelper.retryCount = 3;
+
+  XCTestExpectation* successExpectation = [self expectationWithDescription:@"success"];
+  
+  __block NSUInteger connectionAttempts = 0;
+  __block NSUInteger failedConnections = 0;
+  __block NSUInteger successfulConnections = 0;
+  
+  [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    BOOL foundBranchKey = [request.URL.query rangeOfString:@"branch_key=key_foo"].location != NSNotFound;
+    XCTAssertEqual(foundBranchKey, TRUE);
+    return foundBranchKey;
     
-    // Specify retry count as 3
-    serverInterface.preferenceHelper.retryCount = 3;
+  } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    if (connectionAttempts++ < 3) {
+    // Return an error the first three times
+      NSDictionary* dummyJSONResponse = @{@"bad": @"data"};
 
-    // 3 retries means 4 total requests
-    [self expectSendAsyncRequestForBranchError:urlConnectionMock times:4];
-    
-    // Make the request
-    XCTestExpectation *getRequestExpectation = [self expectationWithDescription:@"GET Request Expectation"];
-    [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:^(BNCServerResponse *response, NSError *error) {
-        XCTAssertNotNil(error);
+      NSLog(@"attempt # %lu", (unsigned long)connectionAttempts);
+      ++failedConnections;
+      return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:504 headers:nil];
+      
+    } else {
+      // Return actual data afterwards
+      ++successfulConnections;
+      NSDictionary* dummyJSONResponse = @{@"key": @"value"};
+      XCTAssertEqual(connectionAttempts, failedConnections + successfulConnections);
+      [successExpectation fulfill];
 
-        [getRequestExpectation fulfill];
-    }];
-    
-    [self waitForExpectationsWithTimeout:5 handler:NULL];
-    
-    [urlConnectionMock verify];*/
-
-    // gangster block
-    OHHTTPStubsResponse* (^datResponse)(NSURLRequest *) = ^OHHTTPStubsResponse*(NSURLRequest* request) {
-        NSDictionary* dummyJSONResponse = @{@"key": @"value"};
-        return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:504 headers:nil];
-    };
-
-    // Specify retry count as 3
-    serverInterface.preferenceHelper.retryCount = 3;
-
-    // 3 retries means 4 total requests
-    [self expectSendAsyncRequestForBranchError:urlConnectionMock times:4];
-
-    XCTestExpectation* expectation = [self expectationWithDescription:@"Get Request Expectation"];
-
-    [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
-        // We're not sending a request, just verifying a "branch_key=key_foo" is present.
-        return [request.URL.query rangeOfString:@"branch_key=key_foo"].location != NSNotFound;
-    } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
-        NSDictionary* dummyJSONResponse = @{@"key": @"value"};
-        return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:200 headers:nil];
-    }];
-
-    [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:NULL];
-
-    [self waitForExpectationsWithTimeout:5.0 /* 5 seconds */ handler:nil];
+      return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:200 headers:nil];
+    }
+  }];
+  
+  [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:NULL];
+  [self waitForExpectationsWithTimeout:5.0 handler:nil];
 }
+
+
+
+//==================================================================================
+//TEST 04
+// This test checks to make sure that GET retries are not attempted when they have a retry
+// count > 0, but retries aren't needed. Based on Test #3 above.
 
 - (void)testGetRequestAsyncRetriesWhenInappropriateResponse {
-    BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
-    serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
-    id urlConnectionMock = OCMClassMock([NSURLConnection class]);
-
-    // Specify retry count as 3
-    serverInterface.preferenceHelper.retryCount = 3;
-
-    // Should be no retries, just a single request
-    [self expectSendAsyncRequestForSuccessfulRequest:urlConnectionMock];
+  BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
+  serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
+  serverInterface.preferenceHelper.retryCount = 3;
+  
+  XCTestExpectation* successExpectation = [self expectationWithDescription:@"success"];
+  
+  __block NSUInteger connectionAttempts = 0;
+  
+  [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    BOOL foundBranchKey = [request.URL.query rangeOfString:@"branch_key=key_foo"].location != NSNotFound;
+    XCTAssertEqual(foundBranchKey, TRUE);
+    return foundBranchKey;
     
-    // Make the request
-    XCTestExpectation *getRequestExpectation = [self expectationWithDescription:@"GET Request Expectation"];
-    [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:^(BNCServerResponse *response, NSError *error) {
-        XCTAssertNil(error);
-        
-        [getRequestExpectation fulfill];
-    }];
-    
-    [self waitForExpectationsWithTimeout:1 handler:NULL];
+  } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+     // Return actual data on first attempt
+     NSDictionary* dummyJSONResponse = @{@"key": @"value"};
+     connectionAttempts++;
+     XCTAssertEqual(connectionAttempts, 1);
+     [successExpectation fulfill];
+     
+     return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:200 headers:nil];
+     
+   }];
+  
+  [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:NULL];
+  [self waitForExpectationsWithTimeout:1.0 handler:nil];
+  
 }
+
+
+
+//==================================================================================
+//TEST 05
+// This test checks to make sure that GET retries are not attempted when they have a retry
+// count == 0, but retries aren't needed. Based on Test #4 above
 
 - (void)testGetRequestAsyncRetriesWhenInappropriateRetryCount {
-    BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
-    serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
-    id urlConnectionMock = OCMClassMock([NSURLConnection class]);
+  
+  BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
+  serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
+  serverInterface.preferenceHelper.retryCount = 0;
+  
+  XCTestExpectation* successExpectation = [self expectationWithDescription:@"success"];
+  
+  __block NSUInteger connectionAttempts = 0;
+  
+  [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    BOOL foundBranchKey = [request.URL.query rangeOfString:@"branch_key=key_foo"].location != NSNotFound;
+    XCTAssertEqual(foundBranchKey, TRUE);
+    return foundBranchKey;
     
-    // Specify retry count as 0
-    serverInterface.preferenceHelper.retryCount = 0;
+  } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    // Return actual data on first attempt
+    NSDictionary* dummyJSONResponse = @{@"key": @"value"};
+    connectionAttempts++;
+    XCTAssertEqual(connectionAttempts, 1);
+    [successExpectation fulfill];
     
-    // 0 retries means 1 total requests
-    [self expectSendAsyncRequestForBranchError:urlConnectionMock times:1];
+    return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:200 headers:nil];
     
-    // Make the request
-    XCTestExpectation *getRequestExpectation = [self expectationWithDescription:@"GET Request Expectation"];
-    [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:^(BNCServerResponse *response, NSError *error) {
-        XCTAssertNotNil(error);
-        
-        [getRequestExpectation fulfill];
-    }];
-    
-    [self waitForExpectationsWithTimeout:1 handler:NULL];
+  }];
+  
+  [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:NULL];
+  [self waitForExpectationsWithTimeout:1.0 handler:nil];
+  
 }
+
+
+
+
+//==================================================================================
+//TEST 06
+//This test simulates a poor network, with three failed GET attempts and one final success,
+// for 4 connections. Based on Test #3 above
 
 - (void)testPostRequestAsyncRetriesWhenAppropriate {
-    BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
-    serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
-    id urlConnectionMock = OCMClassMock([NSURLConnection class]);
+  
+  //Set up nsurlsession and data task, catching response
+  BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
+  serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
+  serverInterface.preferenceHelper.retryCount = 3;
+  
+  XCTestExpectation* successExpectation = [self expectationWithDescription:@"success"];
+  
+  __block NSUInteger connectionAttempts = 0;
+  __block NSUInteger failedConnections = 0;
+  __block NSUInteger successfulConnections = 0;
+  
+  [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    BOOL foundBranchKey = [request.URL.query rangeOfString:@"branch_key=key_foo"].location != NSNotFound;
+    XCTAssertEqual(foundBranchKey, TRUE);
+    return foundBranchKey;
     
-    // Specify retry count as 3
-    serverInterface.preferenceHelper.retryCount = 3;
-    
-    // 3 retries means 4 total requests
-    [self expectSendAsyncRequestForBranchError:urlConnectionMock times:4];
-    
-    // Make the request
-    XCTestExpectation *postRequestExpectation = [self expectationWithDescription:@"POST Request Expectation"];
-    [serverInterface postRequest:nil url:@"http://foo" key:@"key_foo" callback:^(BNCServerResponse *response, NSError *error) {
-        XCTAssertNotNil(error);
-        
-        [postRequestExpectation fulfill];
-    }];
-
-    [self waitForExpectationsWithTimeout:5 handler:NULL];
+  } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    if (connectionAttempts++ < 3) {
+      // Return an error the first three times
+      NSDictionary* dummyJSONResponse = @{@"bad": @"data"};
+      
+      NSLog(@"attempt # %lu", (unsigned long)connectionAttempts);
+      ++failedConnections;
+      return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:504 headers:nil];
+      
+    } else {
+      // Return actual data afterwards
+      ++successfulConnections;
+      NSDictionary* dummyJSONResponse = @{@"key": @"value"};
+      XCTAssertEqual(connectionAttempts, failedConnections + successfulConnections);
+      [successExpectation fulfill];
+      
+      return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:200 headers:nil];
+    }
+  }];
+  
+  [serverInterface postRequest:nil url:@"http://foo" key:@"key_foo" callback:NULL];
+  [self waitForExpectationsWithTimeout:5.0 handler:nil];
 }
+
+
+
+
+
+//==================================================================================
+//TEST 07
+// This test checks to make sure that POST retries are not attempted when they have a retry
+// count == 0, but retries aren't needed. Based on Test #4 above
 
 - (void)testPostRequestAsyncRetriesWhenInappropriateResponse {
-    BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
-    serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
-    id urlConnectionMock = OCMClassMock([NSURLConnection class]);
+  
+  BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
+  serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
+  serverInterface.preferenceHelper.retryCount = 3;
+  
+  XCTestExpectation* successExpectation = [self expectationWithDescription:@"success"];
+  
+  __block NSUInteger connectionAttempts = 0;
+  
+  [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    BOOL foundBranchKey = [request.URL.query rangeOfString:@"branch_key=key_foo"].location != NSNotFound;
+    XCTAssertEqual(foundBranchKey, TRUE);
+    return foundBranchKey;
     
-    // Specify retry count as 3
-    serverInterface.preferenceHelper.retryCount = 3;
+  } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    // Return actual data on first attempt
+    NSDictionary* dummyJSONResponse = @{@"key": @"value"};
+    connectionAttempts++;
+    XCTAssertEqual(connectionAttempts, 1);
+    [successExpectation fulfill];
     
-    // Should be no retries, just a single request
-    [self expectSendAsyncRequestForSuccessfulRequest:urlConnectionMock];
+    return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:200 headers:nil];
     
-    // Make the request
-    XCTestExpectation *postRequestExpectation = [self expectationWithDescription:@"POST Request Expectation"];
-    [serverInterface postRequest:nil url:@"https://foo.com" key:@"key_foo" callback:^(BNCServerResponse *response, NSError *error) {
-        XCTAssertNil(error);
-        [postRequestExpectation fulfill];
-    }];
-    
-    [self waitForExpectationsWithTimeout:1 handler:NULL];
+  }];
+  
+  [serverInterface postRequest:nil url:@"http://foo" key:@"key_foo" callback:NULL];
+  [self waitForExpectationsWithTimeout:1.0 handler:nil];
+  
 }
+
+
+
+
+
+
+
+
+//==================================================================================
+//TEST 08
+// This test checks to make sure that GET retries are not attempted when they have a retry
+// count == 0, but retries aren't needed. Based on Test #4 above
 
 - (void)testPostRequestAsyncRetriesWhenInappropriateRetryCount {
-    BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
-    serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
-    id urlConnectionMock = OCMClassMock([NSURLConnection class]);
+  BNCServerInterface *serverInterface = [[BNCServerInterface alloc] init];
+  serverInterface.preferenceHelper = [[BNCPreferenceHelper alloc] init];
+  serverInterface.preferenceHelper.retryCount = 0;
+  
+  XCTestExpectation* successExpectation = [self expectationWithDescription:@"success"];
+  
+  __block NSUInteger connectionAttempts = 0;
+  
+  [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
+    BOOL foundBranchKey = [request.URL.query rangeOfString:@"branch_key=key_foo"].location != NSNotFound;
+    XCTAssertEqual(foundBranchKey, TRUE);
+    return foundBranchKey;
     
-    // Specify retry count as 0
-    serverInterface.preferenceHelper.retryCount = 0;
+  } withStubResponse:^OHHTTPStubsResponse*(NSURLRequest *request) {
+    // Return actual data on first attempt
+    NSDictionary* dummyJSONResponse = @{@"key": @"value"};
+    connectionAttempts++;
+    XCTAssertEqual(connectionAttempts, 1);
+    [successExpectation fulfill];
     
-    // 0 retries means 1 total requests
-    [self expectSendAsyncRequestForBranchError:urlConnectionMock times:1];
+    return [OHHTTPStubsResponse responseWithJSONObject:dummyJSONResponse statusCode:200 headers:nil];
     
-    // Make the request
-    XCTestExpectation *postRequestExpectation = [self expectationWithDescription:@"POST Request Expectation"];
-    [serverInterface postRequest:nil url:@"http://foo" key:@"key_foo" callback:^(BNCServerResponse *response, NSError *error) {
-        XCTAssertNotNil(error);
-        
-        [postRequestExpectation fulfill];
-    }];
-    
-    [self waitForExpectationsWithTimeout:1 handler:NULL];
+  }];
+  
+  [serverInterface getRequest:nil url:@"http://foo" key:@"key_foo" callback:NULL];
+  [self waitForExpectationsWithTimeout:1.0 handler:nil];
+  
 }
 
+
+
+
+
+
+
+// These appear to be unneeded now
 #pragma mark - Internals
 - (void)expectSendAsyncRequestForBranchError:(id)connectionMock times:(NSInteger)times {
     __block UrlConnectionCallback urlConnectionCallback;
