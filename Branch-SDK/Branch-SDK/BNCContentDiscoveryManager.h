e769c6d68f25d876ae6d36c8a9711d56ae7e31c5
Dan Walkowski <dan.walkowski@gmail.com>
2015-11-20 10:12:06 -0800

    Concerted effort to pass through the expiration date from the partner API to Spotlight.  This required far more changes than it ought to have. In the end, although it -appears- that everything is correctly configured, the items do not get flushed from Spotlight, despite the correct date appearing in the mdItemExpirationDate field of the CSSearchItem. I'm checking this in on a floating branch for further investigation.

--- Branch-SDK/Branch-SDK/BNCContentDiscoveryManager.h
+++ Branch-SDK/Branch-SDK/BNCContentDiscoveryManager.h
@@ -24,5 +24,7 @@
 - (void)indexContentWithTitle:(NSString *)title description:(NSString *)description publiclyIndexable:(BOOL)publiclyIndexable thumbnailUrl:(NSURL *)thumbnailUrl userInfo:(NSDictionary *)userInfo;
 - (void)indexContentWithTitle:(NSString *)title description:(NSString *)description publiclyIndexable:(BOOL)publiclyIndexable thumbnailUrl:(NSURL *)thumbnailUrl keywords:(NSSet *)keywords userInfo:(NSDictionary *)userInfo;
 - (void)indexContentWithTitle:(NSString *)title description:(NSString *)description publiclyIndexable:(BOOL)publiclyIndexable type:(NSString *)type thumbnailUrl:(NSURL *)thumbnailUrl keywords:(NSSet *)keywords userInfo:(NSDictionary *)userInfo callback:(callbackWithUrl)callback;
+- (void)indexContentWithTitle:(NSString *)title description:(NSString *)description publiclyIndexable:(BOOL)publiclyIndexable type:(NSString *)type thumbnailUrl:(NSURL *)thumbnailUrl keywords:(NSSet *)keywords userInfo:(NSDictionary *)userInfo expirationDate:(NSDate *)expirationDate callback:(callbackWithUrl)callback;
+
 
 @end
