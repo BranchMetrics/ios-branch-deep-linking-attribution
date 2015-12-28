141e9abc546e27e48a1ef7c0200dc6a5a9bcea98
Dan Walkowski <dan.walkowski@gmail.com>
2015-12-28 15:03:13 -0800

    • first cut at supporting APNS in Testbed. I might be able to hide more of the code, but many developers will already have handlers in place.

--- Branch-TestBed/Branch-TestBed/AppDelegate.m
+++ Branch-TestBed/Branch-TestBed/AppDelegate.m
@@ -14,11 +14,26 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     ExampleDeepLinkingController *controller = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateViewControllerWithIdentifier:@"DeepLinkingController"];
     
+    
+    //APNS support: different between ios7 and ios8+
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+    {
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
+        
+        [application registerForRemoteNotifications];
+    }
+    else
+    {
+        [application registerForRemoteNotificationTypes: (UIRemoteNotificationTypeNewsstandContentAvailability| UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    }
+    //end APNS support
+    
+    
     Branch *branch = [Branch getInstance];
     [branch setDebug];
     
     [branch registerDeepLinkController:controller forKey:@"gravatar_email"];
-
+    
     [branch initSessionWithLaunchOptions:launchOptions automaticallyDisplayDeepLinkController:YES deepLinkHandler:^(NSDictionary *params, NSError *error) {
         if (!error) {
             NSLog(@"finished init with params = %@", [params description]);
@@ -27,13 +42,39 @@
             NSLog(@"failed init: %@", error);
         }
     }];
-
+    
     return YES;
 }
 
+
+//APNS support
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSLog(@"APN device token: %@", deviceToken);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"Push received: %@", userInfo);
+    //check a branch shortlink in the payload (shortlink because iOS7 only supports 256 bytes)
+    NSString *urlStr = [userInfo objectForKey:@"branchURL"];
+    if (urlStr) {
+        NSURL *branchURL = [NSURL URLWithString:urlStr];
+        [[Branch getInstance] handleDeepLink:branchURL];
+    }
+    
+    //process your other payload items...
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    NSLog(@"Error registering for remote notifications:%@",error);
+}
+//end APNS support
+
+
+
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
     NSLog(@"opened app from URL %@", [url description]);
-
+    
     return [[Branch getInstance] handleDeepLink:url];
 }
 
@@ -42,5 +83,6 @@
     
     return handledByBranch;
 }
+
 
 @end
