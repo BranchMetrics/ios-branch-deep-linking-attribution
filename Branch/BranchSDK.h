c117035bd6664d1a9950f95f84a143a7f10b806e
hov <hov@picsart.com>
2016-02-29 18:59:45 +0400

    added framework project

---
+++ Branch/BranchSDK.h
@@ -0,0 +1,26 @@
+//
+//  Branch
+//
+//  Created by Hovhannes Safaryan on 2/29/16.
+//  Copyright © 2016 Branch. All rights reserved.
+//
+
+#import "Branch.h"
+
+#import "BranchActivityItemProvider.h"
+
+#import "BNCServerInterface.h"
+
+#import "BNCServerRequestQueue.h"
+
+#import "BNCLinkCache.h"
+
+#import "BNCLinkData.h"
+
+#import "BranchDeepLinkingController.h"
+
+#import "BNCPreferenceHelper.h"
+
+#import "BNCServerResponse.h"
+
+
