5b8967bdcd1de3113af49bb9af393a48bfbd303d
Derrick Staten <statenderrick@gmail.com>
2016-03-11 14:50:31 -0800

    clean up the testbed's app delegate

--- Branch-TestBed/Branch-TestBed/AppDelegate.m
+++ Branch-TestBed/Branch-TestBed/AppDelegate.m
@@ -11,73 +11,79 @@
 
 @implementation AppDelegate
 
+#pragma mark - Required methods for Branch SDK integration
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    ExampleDeepLinkingController *controller = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateViewControllerWithIdentifier:@"DeepLinkingController"];
-    
-    
-    //APNS support: different between ios7 and ios8+
-    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
-    {
-        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
-        
-        [application registerForRemoteNotifications];
-    }
-    else
-    {
-        [application registerForRemoteNotificationTypes: (UIRemoteNotificationTypeNewsstandContentAvailability| UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
-    }
-    //end APNS support
-    
+    /**
+     * // Push notification support (Optional)
+     * [self registerForPushNotifications:application];
+     */
     
     Branch *branch = [Branch getInstance];
     [branch setDebug];
     
-    [branch setDeepLinkDebugMode:@{@"example_debug_param" : @"foo"}];
-    
+    // Example of automatic deep link routing (https://dev.branch.io/getting-started/deep-link-routing/guide/ios/)
+    ExampleDeepLinkingController *controller = [[UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateViewControllerWithIdentifier:@"DeepLinkingController"];
     [branch registerDeepLinkController:controller forKey:@"gravatar_email"];
     
+    // Required. Initialize session. automaticallyDisplayDeepLinkController is optional (default is NO).
     [branch initSessionWithLaunchOptions:launchOptions automaticallyDisplayDeepLinkController:YES deepLinkHandler:^(NSDictionary *params, NSError *error) {
         if (!error) {
-            NSLog(@"finished init with params = %@", [params description]);
+            NSLog(@"Branch finished init with params: %@", [params description]);
         }
         else {
-            NSLog(@"failed init: %@", error);
+            NSLog(@"Branch failed init: %@", error);
         }
     }];
     
     return YES;
 }
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    NSLog(@"application:openURL:sourceApplication:annotation: invoked with URL: %@", [url description]);
 
-//APNS support
+    // Required. Returns YES if Branch link, else returns NO
+    [[Branch getInstance] handleDeepLink:url];
+
+    // Process non-Branch URIs here...
+    return YES;
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
+    NSLog(@"application:continueUserActivity:restorationHandler: invoked. userActivity.webpageURL if any: %@", userActivity.webpageURL.absoluteString);
+
+    // Required. Returns YES if Branch Universal Link, else returns NO. Add `branch_universal_link_domains` to .plist (String or Array) for custom domain(s).
+    [[Branch getInstance] continueUserActivity:userActivity];
+
+    // Process non-Branch userActivities here...
+    return YES;
+}
+
+
+#pragma mark - Push Notifications (Optional)
+
+// Helper method
+- (void)registerForPushNotifications:(UIApplication *)application {
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
+        [application registerForRemoteNotifications];
+    }
+    else {
+        [application registerForRemoteNotificationTypes: (UIRemoteNotificationTypeNewsstandContentAvailability| UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    }
+}
+
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    NSLog(@"APN device token: %@", deviceToken);
+    NSLog(@"Registered for remote notifications with APN device token: %@", deviceToken);
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
     [[Branch getInstance] handlePushNotification:userInfo];
-    
-    //process your other notification payload items...
+    // process your non-Branch notification payload items here...
 }
 
 -(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    NSLog(@"Error registering for remote notifications:%@",error);
+    NSLog(@"Error registering for remote notifications: %@", error);
 }
-//end APNS support
-
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-    NSLog(@"opened app from URL %@", [url description]);
-    
-    return [[Branch getInstance] handleDeepLink:url];
-}
-
-- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
-    BOOL handledByBranch = [[Branch getInstance] continueUserActivity:userActivity];
-    
-    return handledByBranch;
-}
-
 
 @end
