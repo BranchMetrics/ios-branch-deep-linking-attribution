2c3d558bc54da036e8f88cf456a8b506f527a2af
David Westgate <david@branchmetrics.io>
2016-07-03 14:06:58 -0700

    Updated-TestBed

---
+++ Branch-TestBed/Branch-TestBed/NavigationController.h
@@ -0,0 +1,14 @@
+//
+//  NavigationController.h
+//  Branch-TestBed
+//
+//  Created by David Westgate on 5/22/16.
+//  Copyright © 2016 Branch Metrics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Branch.h"
+
+@interface NavigationController : UINavigationController <BranchDeepLinkingController>
+
+@end
