146b9b9e37baf59e16e948403ceccd8bd53a1785
David Westgate <david@branchmetrics.io>
2016-07-03 14:06:58 -0700

    Updated-TestBed

---
+++ Branch-TestBed/Branch-TestBed/LogOutputViewController.h
@@ -0,0 +1,16 @@
+//
+//  LogOutputViewController.h
+//  Branch-TestBed
+//
+//  Created by David Westgate on 5/11/16.
+//  Copyright © 2016 Branch Metrics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Branch.h"
+
+@interface LogOutputViewController : UIViewController // <BranchDeepLinkingController>
+
+@property (nonatomic, strong) NSString *logOutput;
+
+@end
