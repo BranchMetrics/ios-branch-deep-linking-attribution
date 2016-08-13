9e2b9929a128f86c6379a47ca52aec80c516f811
Ahmed Nawar <ahmed@branch.io>
2016-08-12 17:59:07 -0700

    add checksumming to the release prorocedure

--- scripts/upload_zips.sh
+++ scripts/upload_zips.sh
@@ -12,5 +12,8 @@ aws s3 cp --acl public-read Branch-iOS-SDK.zip s3://branchhost/
 echo 'Uploading Branch-iOS-TestBed'
 aws s3 cp --acl public-read Branch-iOS-TestBed.zip s3://branchhost/
 
+echo 'Computing the checksums'
+sh scripts/checksum.sh
+
 echo 'Removing zip files'
 rm Branch-iOS-SDK.zip Branch-iOS-TestBed.zip
