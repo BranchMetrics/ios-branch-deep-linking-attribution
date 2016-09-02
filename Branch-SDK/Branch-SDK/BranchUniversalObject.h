9db403b5cf8b781c165b5a789e7a1b783f47bf8f
Ahmed Nawar <ahmed@branch.io>
2016-09-02 14:25:29 -0700

    indexing cleanup

--- Branch-SDK/Branch-SDK/BranchUniversalObject.h
+++ Branch-SDK/Branch-SDK/BranchUniversalObject.h
@@ -60,9 +60,7 @@ typedef NS_ENUM(NSInteger, ContentIndexMode) {
 
 - (void)listOnSpotlight;
 - (void)listOnSpotlightWithCallback:(callbackWithUrl)callback;
-// DEPRECATE  https://dev.branch.io/features/spotlight-indexing/overview/
-//Message: “iOS 10 has changed how Spotlight indexing works and we’ve updated the SDK to reflect this. Please see <link> for instructions on migration"
-- (void)listOnSpotlightWithIdentifierCallback:(callbackWithUrlAndSpotlightIdentifier)spotlightCallback;
+- (void)listOnSpotlightWithIdentifierCallback:(callbackWithUrlAndSpotlightIdentifier)spotlightCallback __attribute__((deprecated(("iOS 10 has changed how Spotlight indexing works and we’ve updated the SDK to reflect this. Please see https://dev.branch.io/features/spotlight-indexing/overview/ for instructions on migration"))));;
 
 // Convenience method for initSession methods that return BranchUniversalObject, but can be used safely by anyone.
 + (BranchUniversalObject *)getBranchUniversalObjectFromDictionary:(NSDictionary *)dictionary;
