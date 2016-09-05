96223a556de4f653b4d3560fef5349f7d486057b
David Westgate <david@branchmetrics.io>
2016-09-05 13:14:01 -0700

    TestBed-SwiftV2

--- Branch-TestBed-Swift/TestBed-Swift/NavigationController.swift
+++ Branch-TestBed-Swift/TestBed-Swift/NavigationController.swift
@@ -2,10 +2,9 @@
 //  NavigationController.swift
 //  TestBed-Swift
 //
-//  Created by David Westgate on 5/27/16.
+//  Created by David Westgate on 8/29/16.
 //  Copyright © 2016 Branch Metrics. All rights reserved.
 //
-
 import UIKit
 
 class NavigationController: UINavigationController, BranchDeepLinkingController {
@@ -17,21 +16,18 @@ class NavigationController: UINavigationController, BranchDeepLinkingController 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    
-    func configureControlWithData(data: [NSObject : AnyObject]!) {
+    func configureControlWithData(params: [NSObject : AnyObject]!) {
         let logOutputViewController = self.storyboard?.instantiateViewControllerWithIdentifier("LogOutput") as! LogOutputViewController
         self.pushViewController(logOutputViewController, animated: true)
-        if let deeplinkText = data["deeplink_text"] as! String? {
-            let logOutput = String(format:"Successfully Deeplinked:\n\n%@\nSession Details:\n\n%@", deeplinkText, data.description)
-            logOutputViewController.logOutput = logOutput
-        }
+        
+        let dict = params as Dictionary
+        let referringLink = dict["~referring_link"]
+        let logOutput = String(format:"\nReferring link: \(referringLink)\n\nSessionDetails:\n\(dict.JSONDescription())")
+        logOutputViewController.logOutput = logOutput
     }
-    
-
 }
---
+++ Branch-TestBed-Swift/TestBed-Swift/TestData.swift
@@ -0,0 +1,132 @@
+//
+//  TestData.swift
+//  TestBed-Swift
+//
+//  Created by David Westgate on 8/29/16.
+//  Copyright © 2016 Branch Metrics. All rights reserved.
+//
+import Foundation
+
+struct TestData {
+    
+    static let userDefaults = NSUserDefaults.standardUserDefaults()
+
+    static func getUserID() -> String? {
+        if let value = userDefaults.stringForKey("userID") {
+            return value
+        } else {
+            let value = ""
+            userDefaults.setValue(value, forKey: "userID")
+            return value
+        }
+    }
+    
+    static func setUserID(value: String) {
+        userDefaults.setValue(value, forKey: "userID")
+    }
+    
+    static func getLinkProperties() -> [String: AnyObject] {
+        if let value = userDefaults.dictionaryForKey("linkProperties") {
+            return value
+        } else {
+            let value = [String: AnyObject]()
+            userDefaults.setObject(value, forKey: "linkProperties")
+            return value
+        }
+    }
+    
+    static func setLinkProperties(value: [String: AnyObject]) {
+        userDefaults.setObject(value, forKey: "linkProperties")
+    }
+    
+    static func getUniversalObjectProperties() -> [String: AnyObject] {
+        if let value = userDefaults.dictionaryForKey("UniversalObjectProperties") {
+            return value
+        } else {
+            let value = [String: AnyObject]()
+            userDefaults.setObject(value, forKey: "UniversalObjectProperties")
+            return value
+        }
+    }
+    
+    static func setUniversalObjectProperties(value: [String: AnyObject]) {
+        userDefaults.setObject(value, forKey: "UniversalObjectProperties")
+    }
+    
+    static func getRewardsBucket() -> String {
+        if let value = userDefaults.stringForKey("rewardsBucket") {
+            return value
+        } else {
+            let value = ""
+            userDefaults.setValue(value, forKey: "rewardsBucket")
+            return value
+        }
+    }
+    
+    static func setRewardsBucket(value: String) {
+        userDefaults.setValue(value, forKey: "rewardsBucket")
+    }
+    
+    static func getRewardsBalanceOfBucket() -> String {
+        if let value = userDefaults.stringForKey("rewardsBalanceOfBucket") {
+            return value
+        } else {
+            let value = ""
+            userDefaults.setValue(value, forKey: "rewardsBalanceOfBucket")
+            return value
+        }
+    }
+    
+    static func setRewardsBalanceOfBucket(value: String) {
+        userDefaults.setValue(value, forKey: "rewardsBalanceOfBucket")
+    }
+    
+    static func getRewardPointsToRedeem() -> String {
+        if let value = userDefaults.stringForKey("rewardPointsToRedeem") {
+            return value
+        } else {
+            let value = ""
+            userDefaults.setValue(value, forKey: "rewardPointsToRedeem")
+            return value
+        }
+    }
+    
+    static func setRewardPointsToRedeem(value: String) {
+        if Int(value) != nil {
+            userDefaults.setValue(value, forKey: "rewardPointsToRedeem")
+        } else {
+            userDefaults.setValue("", forKey: "rewardPointsToRedeem")
+        }
+    }
+    
+    static func getCustomEventName() -> String? {
+        if let value = userDefaults.stringForKey("customEventName") {
+            return value
+        } else {
+            let value = ""
+            userDefaults.setValue(value, forKey: "customEventName")
+            return value
+        }
+    }
+    
+    static func setCustomEventName(value: String) {
+        userDefaults.setValue(value, forKey: "customEventName")
+    }
+    
+    static func getCustomEventMetadata() -> [String: AnyObject] {
+        if let value = userDefaults.dictionaryForKey("customEventMetadata") {
+            return value
+        } else {
+            let value = [String: AnyObject]()
+            userDefaults.setObject(value, forKey: "customEventMetadata")
+            return value
+        }
+    }
+    
+    static func setCustomEventMetadata(value: [String: AnyObject]) {
+        userDefaults.setObject(value, forKey: "customEventMetadata")
+    }
+    
+    
+    
+}
