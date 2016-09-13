42b5817ea3015b6bd45a7ebe9109d1f5343657ed
sojan <sojan@branch.io>
2016-09-13 13:25:52 -0700

    Updates for 0.12.8 release.

--- Branch.framework/Versions/A/Headers/BranchUniversalObject.h
+++ Branch.framework/Versions/A/Headers/BranchUniversalObject.h
@@ -11,7 +11,8 @@
 
 @class BranchLinkProperties;
 
-typedef void (^shareCompletion) (NSString *_Nonnull activityType, BOOL completed);
+typedef void (^shareCompletion) (NSString * _Nullable activityType, BOOL completed);
+typedef void (^shareCompletionWithError) (NSString * _Nullable activityType, BOOL completed, NSError * _Nullable activityError);
 
 typedef NS_ENUM(NSInteger, ContentIndexMode) {
     ContentIndexModePublic,
@@ -20,51 +21,55 @@ typedef NS_ENUM(NSInteger, ContentIndexMode) {
 
 @interface BranchUniversalObject : NSObject
 
-@property (nonatomic, strong) NSString *canonicalIdentifier;
-@property (nonatomic, strong) NSString *canonicalUrl;
-@property (nonatomic, strong) NSString *title;
-@property (nonatomic, strong) NSString *contentDescription;
-@property (nonatomic, strong) NSString *imageUrl;
+@property (nonatomic, strong, nonnull) NSString *canonicalIdentifier;
+@property (nonatomic, strong, nullable) NSString *canonicalUrl;
+@property (nonatomic, strong, nullable) NSString *title;
+@property (nonatomic, strong, nullable) NSString *contentDescription;
+@property (nonatomic, strong, nullable) NSString *imageUrl;
 // Note: properties found in metadata will overwrite properties on the BranchUniversalObject itself
-@property (nonatomic, strong) NSDictionary *metadata;
-@property (nonatomic, strong) NSString *type;
+@property (nonatomic, strong, nullable) NSDictionary *metadata;
+@property (nonatomic, strong, nullable) NSString *type;
 @property (nonatomic) ContentIndexMode contentIndexMode;
-@property (nonatomic, strong) NSArray *keywords;
-@property (nonatomic, strong) NSDate *expirationDate;
-@property (nonatomic, strong) NSString *spotlightIdentifier;
+@property (nonatomic, strong, nullable) NSArray *keywords;
+@property (nonatomic, strong, nullable) NSDate *expirationDate;
+@property (nonatomic, strong, nullable) NSString *spotlightIdentifier;
 @property (nonatomic, assign) CGFloat price;
-@property (nonatomic, strong) NSString *currency;
+@property (nonatomic, strong, nullable) NSString *currency;
 @property (nonatomic, assign) BOOL automaticallyListOnSpotlight;
 
 
-- (instancetype)initWithCanonicalIdentifier:(NSString *)canonicalIdentifier;
-- (instancetype)initWithTitle:(NSString *)title;
+- (nullable instancetype)initWithCanonicalIdentifier:(nonnull NSString *)canonicalIdentifier;
+- (nullable instancetype)initWithTitle:(nonnull NSString *)title;
 
-- (void)addMetadataKey:(NSString *)key value:(NSString *)value;
+- (void)addMetadataKey:(nonnull NSString *)key value:(nonnull NSString *)value;
 
 - (void)registerView;
-- (void)registerViewWithCallback:(callbackWithParams)callback;
+- (void)registerViewWithCallback:(nullable callbackWithParams)callback;
 
-- (void)userCompletedAction:(NSString *)action;
+- (void)userCompletedAction:(nonnull NSString *)action;
 
-- (NSString *)getShortUrlWithLinkProperties:(BranchLinkProperties *)linkProperties;
-- (NSString *)getShortUrlWithLinkPropertiesAndIgnoreFirstClick:(BranchLinkProperties *)linkProperties;
-- (void)getShortUrlWithLinkProperties:(BranchLinkProperties *)linkProperties andCallback:(callbackWithUrl)callback;
+- (nullable NSString *)getShortUrlWithLinkProperties:(nonnull BranchLinkProperties *)linkProperties;
+- (nullable NSString *)getShortUrlWithLinkPropertiesAndIgnoreFirstClick:(nonnull BranchLinkProperties *)linkProperties;
+- (void)getShortUrlWithLinkProperties:(nonnull BranchLinkProperties *)linkProperties andCallback:(nonnull callbackWithUrl)callback;
 
-- (UIActivityItemProvider *)getBranchActivityItemWithLinkProperties:(BranchLinkProperties *)linkProperties;
+- (nullable UIActivityItemProvider *)getBranchActivityItemWithLinkProperties:(nonnull BranchLinkProperties *)linkProperties;
 
-- (void)showShareSheetWithShareText:(NSString *)shareText completion:(shareCompletion)completion;
-- (void)showShareSheetWithLinkProperties:(BranchLinkProperties *)linkProperties andShareText:(NSString *)shareText fromViewController:(UIViewController *)viewController completion:(shareCompletion)completion;
+- (void)showShareSheetWithShareText:(nullable NSString *)shareText completion:(nullable shareCompletion)completion;
+- (void)showShareSheetWithLinkProperties:(nullable BranchLinkProperties *)linkProperties andShareText:(nullable NSString *)shareText fromViewController:(nullable UIViewController *)viewController completion:(nullable shareCompletion)completion;
+// Returns with activityError as well
+- (void)showShareSheetWithLinkProperties:(nullable BranchLinkProperties *)linkProperties andShareText:(nullable NSString *)shareText fromViewController:(nullable UIViewController *)viewController completionWithError:(nullable shareCompletionWithError)completion;
 //iPad
-- (void)showShareSheetWithLinkProperties:(BranchLinkProperties *)linkProperties andShareText:(NSString *)shareText fromViewController:(UIViewController *)viewController anchor:(UIBarButtonItem *)anchor completion:(shareCompletion)completion;
+- (void)showShareSheetWithLinkProperties:(nullable BranchLinkProperties *)linkProperties andShareText:(nullable NSString *)shareText fromViewController:(nullable UIViewController *)viewController anchor:(nullable UIBarButtonItem *)anchor completion:(nullable shareCompletion)completion;
+// Returns with activityError as well
+- (void)showShareSheetWithLinkProperties:(nullable BranchLinkProperties *)linkProperties andShareText:(nullable NSString *)shareText fromViewController:(nullable UIViewController *)viewController anchor:(nullable UIBarButtonItem *)anchor completionWithError:(nullable shareCompletionWithError)completion;
 
 - (void)listOnSpotlight;
-- (void)listOnSpotlightWithCallback:(callbackWithUrl)callback;
-- (void)listOnSpotlightWithIdentifierCallback:(callbackWithUrlAndSpotlightIdentifier)spotlightCallback __attribute__((deprecated(("iOS 10 has changed how Spotlight indexing works and we’ve updated the SDK to reflect this. Please see https://dev.branch.io/features/spotlight-indexing/overview/ for instructions on migration"))));;
+- (void)listOnSpotlightWithCallback:(nullable callbackWithUrl)callback;
+- (void)listOnSpotlightWithIdentifierCallback:(nullable callbackWithUrlAndSpotlightIdentifier)spotlightCallback __attribute__((deprecated(("iOS 10 has changed how Spotlight indexing works and we’ve updated the SDK to reflect this. Please see https://dev.branch.io/features/spotlight-indexing/overview/ for instructions on migration"))));;
 
 // Convenience method for initSession methods that return BranchUniversalObject, but can be used safely by anyone.
-+ (BranchUniversalObject *)getBranchUniversalObjectFromDictionary:(NSDictionary *)dictionary;
++ (nonnull BranchUniversalObject *)getBranchUniversalObjectFromDictionary:(nonnull NSDictionary *)dictionary;
 
-- (NSString *)description;
+- (nonnull NSString *)description;
 
 @end
