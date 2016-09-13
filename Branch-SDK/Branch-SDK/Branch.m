5ec8bd5bda8fce10093305e586e1627d8256834a
Alex Austin <alexjaustin@gmail.com>
2016-09-13 09:50:48 -0700

    Clear timer on active

--- Branch-SDK/Branch-SDK/Branch.m
+++ Branch-SDK/Branch-SDK/Branch.m
@@ -1094,6 +1094,7 @@ NSString * const BNCShareCompletedEvent = @"Share Completed";
 #pragma mark - Application State Change methods
 
 - (void)applicationDidBecomeActive {
+    [self clearTimer];
     if (!self.isInitialized && !self.preferenceHelper.shouldWaitForInit && ![self.requestQueue containsInstallOrOpen]) {
         [self initUserSessionAndCallCallback:YES];
     }
