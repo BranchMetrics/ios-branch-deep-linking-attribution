ff507d175b0c98abe6c5301c0df2662e548a72ce
Edward Smith <esmith@branch.io>
2016-10-26 09:11:43 -0700

    Added NSLocale category to support Xcode 7.

---
+++ Branch-SDK/Branch-SDK/BNCXcode7Support.h
@@ -0,0 +1,23 @@
+//
+//  BNCXcode7Support.h
+//  Branch-TestBed
+//
+//  Created by Edward on 10/26/16.
+//  Copyright © 2016 Branch Metrics. All rights reserved.
+//
+
+
+#if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED < 100000
+#warning Compiling with Xcode 7 support
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSLocale (BranchXcode7Support)
+- (NSString*) countryCode;
+- (NSString*) languageCode;
+@end
+
+
+#endif
