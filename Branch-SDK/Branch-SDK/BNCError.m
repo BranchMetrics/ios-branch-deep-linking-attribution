7bfecf4a8863e632bf4fba911186e6a1a6f5a784
Alex Austin <alexjaustin@gmail.com>
2016-11-06 19:55:13 -0800

    revert error changes

--- Branch-SDK/Branch-SDK/BNCError.m
+++ Branch-SDK/Branch-SDK/BNCError.m
@@ -10,45 +10,8 @@
 #import "BNCError.h"
 
 
-NSString * const BNCErrorDomain = @"io.branch.error";
+NSString * const BNCErrorDomain = @"io.branch";
 
+@implementation BNCError
 
-#define _countof(arr) (sizeof(arr) / sizeof(arr[0]))
-
-
-NSError *_Nonnull BNCErrorWithCode(BNCErrorCode errorCode) {
-    return BNCErrorWithCodeAndReason(errorCode, nil);
-}
-
-
-NSError *_Nonnull BNCErrorWithCodeAndReason(BNCErrorCode errorCode, NSString* reason) {
-
-    NSString *const localizedDescriptions[] =
-        {
-         nil    //  0
-        ,nil
-        ,nil
-        ,nil
-        ,nil
-        ,nil    //  5
-        ,nil
-        ,@"ADClient is not available."
-        ,@"No results where found."
-        };
-
-    NSString * description = nil;
-    NSInteger index = errorCode - BNCInitError;
-    if (index >= 0 && index < _countof(localizedDescriptions))
-        description = localizedDescriptions[index];
-
-    NSMutableDictionary *userInfo = [NSMutableDictionary new];
-    if (description)
-        userInfo[NSLocalizedDescriptionKey] = description;
-    if (reason)
-        userInfo[NSLocalizedFailureReasonErrorKey] = reason;
-
-    NSError *error = [NSError errorWithDomain:BNCErrorDomain
-                            code:errorCode
-                        userInfo:userInfo];
-    return error;
-}
+@end
