fdfac85949a342743934dbd59ab2b17efd54ac09
Alex Austin <alexjaustin@gmail.com>
2016-11-06 19:55:13 -0800

    revert error changes

--- Branch-SDK/Branch-SDK/BNCError.h
+++ Branch-SDK/Branch-SDK/BNCError.h
@@ -12,25 +12,16 @@
 
 FOUNDATION_EXPORT NSString *_Nonnull const BNCErrorDomain;
 
-
-typedef NS_ENUM(NSInteger, BNCErrorCode) {
-
-     BNCInitError                   = 1000
-    ,BNCDuplicateResourceError      = 1001
-    ,BNCRedeemCreditsError          = 1002
-    ,BNCBadRequestError             = 1003
-    ,BNCServerProblemError          = 1004
-    ,BNCNilLogError                 = 1005
-    ,BNCVersionError                = 1006
-    ,BNCErrorADClientNotAvailable   = 1007
-    ,BNCErrorNoResults              = 1008
-
+enum {
+    BNCInitError = 1000,
+    BNCDuplicateResourceError,
+    BNCRedeemCreditsError,
+    BNCBadRequestError,
+    BNCServerProblemError,
+    BNCNilLogError,
+    BNCVersionError
 };
 
+@interface BNCError : NSObject
 
-///@param errorCode The Branch error code of the error.
-NSError *_Nonnull BNCErrorWithCode(BNCErrorCode errorCode);
-
-///@param errorCode The Branch error code of the error.
-///@param reason    The reason string for the error.
-NSError *_Nonnull BNCErrorWithCodeAndReason(BNCErrorCode errorCode, NSString*_Nullable reason);
+@end
