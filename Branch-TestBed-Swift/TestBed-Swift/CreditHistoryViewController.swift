e4ccae630ed4de36c56b031643f11a3a7984fc3f
David Westgate <david@branchmetrics.io>
2016-12-27 15:57:20 -0800

    Updated TestBed-Swift

--- Branch-TestBed-Swift/TestBed-Swift/CreditHistoryViewController.swift
+++ Branch-TestBed-Swift/TestBed-Swift/CreditHistoryViewController.swift
@@ -82,10 +82,7 @@ class CreditHistoryViewController: UITableViewController {
             let dateString = transaction["date"] as! String
             let dateFormatter = DateFormatter()
 
-            print(dateString)
             dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-            let test = dateFormatter.date(from: dateString)
-            print(test)
             if let date = dateFormatter.date(from: dateString) {
                 dateFormatter.dateStyle = .medium
                 dateFormatter.timeStyle = .medium
@@ -98,51 +95,5 @@ class CreditHistoryViewController: UITableViewController {
         
         return cell!
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
