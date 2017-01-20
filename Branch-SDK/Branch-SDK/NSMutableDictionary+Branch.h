0a0ba4c593c2a41afb3ba0cca213c9250c6914a6
Edward Smith <esmith@branch.io>
2017-01-20 00:51:55 -0800

    Added NSMutableDictionary+Branch.{h|m} that safely adds values to a dictionary.

---
+++ Branch-SDK/Branch-SDK/NSMutableDictionary+Branch.h
@@ -0,0 +1,18 @@
+//
+//  NSMutableDictionary+Branch.h
+//  Branch
+//
+//  Created by Edward Smith on 1/11/17.
+//  Copyright © 2017 Branch Metrics. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSMutableDictionary (Branch)
+
+- (void) bnc_safeSetObject:(id)anObject forKey:(id<NSCopying>)aKey;
+- (void) bnc_safeAddEntriesFromDictionary:(NSDictionary<id<NSCopying>,id> *)otherDictionary;
+
+@end
---
+++ Branch-SDK/Branch-SDK/NSMutableDictionary+Branch.m
@@ -0,0 +1,27 @@
+//
+//  NSMutableDictionary+Branch.m
+//  Branch
+//
+//  Created by Edward Smith on 1/11/17.
+//  Copyright © 2017 Branch Metrics. All rights reserved.
+//
+
+
+#import "NSMutableDictionary+Branch.h"
+
+
+@implementation NSMutableDictionary (Branch)
+
+- (void) bnc_safeSetObject:(id)anObject forKey:(id<NSCopying>)aKey {
+	if (anObject) {
+		[self setObject:anObject forKey:aKey];
+	}
+}
+
+- (void) bnc_safeAddEntriesFromDictionary:(NSDictionary<id<NSCopying>,id> *)otherDictionary {
+    if ([otherDictionary isKindOfClass:[NSDictionary class]]) {
+        [self addEntriesFromDictionary:otherDictionary];
+    }
+}
+
+@end
