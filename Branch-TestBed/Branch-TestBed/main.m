f3023aad2ac8f78a2e46483ef34cc04e804dec0c
Edward Smith <esmith@branch.io>
2017-09-12 16:11:45 -0700

    Fixed iOS 7 open link non-running app in testbed (testbed was initialing wrong).

--- Branch-TestBed/Branch-TestBed/main.m
+++ Branch-TestBed/Branch-TestBed/main.m
@@ -6,10 +6,8 @@
 //  Copyright (c) 2014 Branch Metrics. All rights reserved.
 //
 
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
