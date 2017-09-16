f39d53cca38804acec138f5f40f33c9ca60d1a9b
rmpardee <ruthmpardee@gmail.com>
2017-09-16 14:26:25 -0700

    Adds deeplinking validator logic

--- Branch-SDK/Branch-SDK/Branch.m
+++ Branch-SDK/Branch-SDK/Branch.m
@@ -1844,10 +1844,89 @@ void BNCPerformBlockOnMainThread(dispatch_block_t block) {
     }
 }
 
+- (void)returnToBrowserBasedOnReferringLink:(NSString *)referringLink currentTest:(NSString *)currentTest newTestVal:(NSString *)val {
+    // TODO: handling for missing ~referring_link
+    // TODO: test with short url where, say, t1=b is set in deep link data. If this logic fails then we'll need to generate a new short URL, which is sucky.
+    NSURLComponents *comp = [NSURLComponents componentsWithURL:[NSURL URLWithString:referringLink]
+                                       resolvingAgainstBaseURL:NO];
+    NSArray *queryParams = [comp queryItems];
+    NSMutableArray *newQueryParams = [NSMutableArray array];
+    for (NSURLQueryItem *queryParam in queryParams) {
+        if (![queryParam.name isEqualToString:currentTest]) {
+            [newQueryParams addObject:queryParam];
+        }
+    }
+    [newQueryParams addObject:[NSURLQueryItem queryItemWithName:currentTest value:val]];
+    [newQueryParams addObject:[NSURLQueryItem queryItemWithName:@"validate" value:@"true"]];
+    comp.queryItems = newQueryParams;
+    [[UIApplication sharedApplication] openURL:comp.URL];
+}
+
+- (UIViewController*)topViewController {
+    return [self topViewControllerWithRootViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
+}
+
+- (UIViewController*)topViewControllerWithRootViewController:(UIViewController*)rootViewController {
+    if ([rootViewController isKindOfClass:[UITabBarController class]]) {
+        UITabBarController* tabBarController = (UITabBarController*)rootViewController;
+        return [self topViewControllerWithRootViewController:tabBarController.selectedViewController];
+    }
+    else if ([rootViewController isKindOfClass:[UINavigationController class]]) {
+        UINavigationController* navigationController = (UINavigationController*)rootViewController;
+        return [self topViewControllerWithRootViewController:navigationController.visibleViewController];
+    }
+    else if (rootViewController.presentedViewController) {
+        UIViewController* presentedViewController = rootViewController.presentedViewController;
+        return [self topViewControllerWithRootViewController:presentedViewController];
+    }
+    else {
+        return rootViewController;
+    }
+}
+
+- (void)rebarWithParams:(NSDictionary *)params {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Rebar Support" message:nil preferredStyle:UIAlertControllerStyleAlert];
+
+                if ([params[@"+clicked_branch_link"] isEqualToNumber:@YES]) {
+                    alertController.message = @"Good news - we got link data. Now a question for you, astute developer: did the app deep link to the specific piece of content you expected to see?";
+                    // yes
+                    [alertController addAction:[UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    [self returnToBrowserBasedOnReferringLink:params[@"~referring_link"] currentTest:params[@"ct"] newTestVal:@"g"];
+                    }]];
+                    // no
+                    [alertController addAction:[UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+                    [self returnToBrowserBasedOnReferringLink:params[@"~referring_link"] currentTest:params[@"ct"] newTestVal:@"r"];
+                    }]];
+                    // cancel
+                    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+                }
+                else {
+                    alertController.message = @"Bummer. It seems like +clicked_branch_link is false - we didn't deep link.  Double check that the link you're clicking has the same branch_key that is being used in your .plist file. Return to Safari when you're ready to test again.";
+                    [alertController addAction:[UIAlertAction actionWithTitle:@"Got it" style:UIAlertActionStyleDefault handler:nil]];
+                }
+
+            [[self topViewController] presentViewController:alertController animated:YES completion:nil];
+        });
+
+    });
+}
+
 - (void)handleInitSuccess {
 
     self.isInitialized = YES;
     NSDictionary *latestReferringParams = [self getLatestReferringParams];
+
+    if ([latestReferringParams[@"_branch_validate"] isEqualToString:@"060514"]) {
+        [self rebarWithParams:latestReferringParams];
+    }
+    else if (([latestReferringParams[@"validate"] isEqualToString:@"true"])) {
+        NSURLComponents *comp = [NSURLComponents componentsWithURL:[NSURL URLWithString:latestReferringParams[@"~referring_link"]]
+                                           resolvingAgainstBaseURL:NO];
+        [[UIApplication sharedApplication] openURL:comp.URL];
+    }
+
     if (self.shouldCallSessionInitCallback) {
         if (self.sessionInitWithParamsCallback) {
             self.sessionInitWithParamsCallback(latestReferringParams, nil);
