efe9e95986aab7c8b66856a3ab1f5fc64e1d2789
Jimmy Dee <jgvdthree@gmail.com>
2017-09-21 13:04:16 -0700

    WebviewExample updates (#717)
    * Added Fastlane to WebViewExample and updated settings for the app
    * Updates for Xcode 9/Swift 4

--- Examples/WebViewExample/WebViewExample/AppDelegate.swift
+++ Examples/WebViewExample/WebViewExample/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var navigationController: NavigationController!
-    var branch: Branch!
 
     // MARK: - UIApplicationDelegate methods
 
@@ -27,16 +26,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          * schema.
          */
         #if USE_BRANCH_TEST_INSTANCE
-            branch = Branch.getTestInstance()
-        #else
-            branch = Branch.getInstance()
+            Branch.setUseTestBranchKey(true)
         #endif
 
         // Store the NavigationController for later link routing.
         navigationController = window?.rootViewController as? NavigationController
 
         // Initialize Branch SDK
-        branch.initSession(launchOptions: launchOptions) {
+        Branch.getInstance().initSession(launchOptions: launchOptions) {
             (buo: BranchUniversalObject?, linkProperties: BranchLinkProperties?, error: Error?) in
             guard error == nil else {
                 BNCLogError("Error from Branch: \(error!)")
@@ -51,11 +48,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        return branch.application(app, open: url, options: options)
+        return Branch.getInstance().application(app, open: url, options: options)
     }
     
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
-        return branch.continue(userActivity)
+        return Branch.getInstance().continue(userActivity)
     }
 
     // MARK: - Branch link routing
