82ec4aeffb447280aade7079d66227e83586e9da
agrimn <anigam@branch.io>
2017-12-18 14:42:39 -0800

    Fixed the Applink verification endpoint

---
+++ Branch-SDK/Branch-SDK/BNCValidator.h
@@ -0,0 +1,14 @@
+//
+//  BNCValidator.h
+//  Branch
+//
+//  Created by agrim on 12/18/17.
+//  Copyright © 2017 Branch, Inc. All rights reserved.
+//
+
+#ifndef BNCValidator_h
+#define BNCValidator_h
+
+
+
+#endif /* BNCValidator_h */
