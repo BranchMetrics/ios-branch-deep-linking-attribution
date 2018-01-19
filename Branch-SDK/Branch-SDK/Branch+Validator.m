72a9ffaf9153b08913b3d2771a9c12f44ab49bb3
agrimn <anigam@branch.io>
2018-01-18 16:27:00 -0800

    Using the SDK's function to get the current View controller. Now popping an alert for SDK verification test

--- Branch-SDK/Branch-SDK/Branch+Validator.m
+++ Branch-SDK/Branch-SDK/Branch+Validator.m
@@ -18,49 +18,70 @@ void BNCForceBranchValidatorCategoryToLoad(void) {
 
 - (void)validateSDKIntegrationCore {
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        BNCPreferenceHelper *preferenceHelper = [BNCPreferenceHelper preferenceHelper];
-        NSString *endpoint = [BRANCH_REQUEST_ENDPOINT_APP_LINK_SETTINGS stringByAppendingPathComponent:preferenceHelper.lastRunBranchKey];
-        [[[BNCServerInterface alloc] init] getRequest:nil url:[preferenceHelper getAPIURL:endpoint] key:nil callback:^(BNCServerResponse *response, NSError *error) {
-            if (error) {
-                NSLog(@"Error with debugIntegration(): %@", error);
-            }
-            else {
-                NSString *passString = @"PASS";
-                NSString *errorString = @"ERROR";
-                
-                NSLog(@"** Initiating Branch integration verification **");
-                NSLog(@"-------------------------------------------------");
-                
-                NSLog(@"------ checking for URI scheme correctness ------");
-                NSString *serverUriScheme = response.data[@"ios_uri_scheme"];
-                NSString *clientUriScheme = [NSString stringWithFormat:@"%@%@", [BNCSystemObserver getDefaultUriScheme], @"://"];
-                NSString *uriScheme = [serverUriScheme isEqualToString:clientUriScheme] ? passString : errorString;
-                NSLog(@"%@: Dashboard Link Settings page '%@' compared to client side '%@'", uriScheme, serverUriScheme, clientUriScheme);
-                NSLog(@"-------------------------------------------------");
-                
-                NSLog(@"-- checking for bundle identifier correctness ---");
-                NSString *serverBundleIdentifier = response.data[@"ios_bundle_id"];
-                NSString *clientBundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
-                NSString *bundleIdentifier = [serverBundleIdentifier isEqualToString:clientBundleIdentifier] ? passString : errorString;
-                NSLog(@"%@: Dashboard Link Settings page '%@' compared to client side '%@'", bundleIdentifier, serverBundleIdentifier, clientBundleIdentifier);
-                NSLog(@"-------------------------------------------------");
-                
-                NSLog(@"----- checking for iOS Team ID correctness ------");
-                NSString *serverTeamId = response.data[@"ios_team_id"];
-                NSString *clientTeamId = [BNCSystemObserver getTeamIdentifier];
-                if ([serverTeamId isEqualToString:clientTeamId]) {
-                    NSLog(@"%@: Dashboard Link Settings page '%@' compared to client side '%@'.", passString, serverTeamId, clientTeamId);
-                }
-                else {
-                    NSLog(@"%@: server side '%@' compared to client side '%@'.", errorString, serverTeamId, clientTeamId);
-                    NSLog(@"To fix your Dashboard settings head over to https://branch.app.link/link-settings-page");
-                    NSLog(@"If you see a null value on the client side, please temporarily add the following key-value pair to your plist: \n\t<key>AppIdentifierPrefix</key><string>$(AppIdentifierPrefix)</string>\n-> then re-run this test.");
-                }
-                NSLog(@"-------------------------------------------------");
-            }
-        }];
-        
+        [self startValidation];
     });
+}
+
+- (void) startValidation {
+    BNCPreferenceHelper *preferenceHelper = [BNCPreferenceHelper preferenceHelper];
+    NSString *endpoint = [BRANCH_REQUEST_ENDPOINT_APP_LINK_SETTINGS stringByAppendingPathComponent:preferenceHelper.lastRunBranchKey];
+    [[[BNCServerInterface alloc] init] getRequest:nil url:[preferenceHelper getAPIURL:endpoint] key:nil callback:^(BNCServerResponse *response, NSError *error) {
+        if (error) {
+            NSLog(@"Error with debugIntegration(): %@", error);
+        }
+        else {
+            NSString *passString = @"PASS";
+            NSString *errorString = @"ERROR";
+            
+            NSLog(@"** Initiating Branch integration verification **");
+            NSLog(@"-------------------------------------------------");
+            
+            NSLog(@"------ checking for URI scheme correctness ------");
+            NSString *serverUriScheme = response.data[@"ios_uri_scheme"];
+            NSString *clientUriScheme = [NSString stringWithFormat:@"%@%@", [BNCSystemObserver getDefaultUriScheme], @"://"];
+            NSString *uriScheme = [serverUriScheme isEqualToString:clientUriScheme] ? passString : errorString;
+            NSString *uriSchemeMessage = [NSString stringWithFormat:@"%@: Dashboard Link Settings page '%@' compared to client side '%@'", uriScheme, serverUriScheme, clientUriScheme];
+            NSLog(@"%@",uriSchemeMessage);
+            NSLog(@"-------------------------------------------------");
+            
+            NSLog(@"-- checking for bundle identifier correctness ---");
+            NSString *serverBundleIdentifier = response.data[@"ios_bundle_id"];
+            NSString *clientBundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+            NSString *bundleIdentifier = [serverBundleIdentifier isEqualToString:clientBundleIdentifier] ? passString : errorString;
+            NSString *bundleIdentifierMessage = [NSString stringWithFormat:@"%@: Dashboard Link Settings page '%@' compared to client side '%@'", bundleIdentifier, serverBundleIdentifier, clientBundleIdentifier];
+            NSLog(@"%@",bundleIdentifierMessage);
+            NSLog(@"-------------------------------------------------");
+            
+            NSLog(@"----- checking for iOS Team ID correctness ------");
+            NSString *serverTeamId = response.data[@"ios_team_id"];
+            NSString *clientTeamId = [BNCSystemObserver getTeamIdentifier];
+            NSString *teamID = [serverTeamId isEqualToString:clientTeamId] ? passString : errorString;
+            NSString *teamIDMessage = [NSString stringWithFormat:@"%@: Dashboard Link Settings page '%@' compared to client side '%@'", teamID, serverTeamId, clientTeamId];
+            NSLog(@"%@",teamIDMessage);
+            NSLog(@"-------------------------------------------------");
+            
+            if ([teamID isEqualToString:errorString] || [bundleIdentifier isEqualToString:errorString] || [uriScheme isEqualToString:errorString] ) {
+                NSLog(@"%@: server side '%@' compared to client side '%@'.", errorString, serverTeamId, clientTeamId);
+                NSLog(@"To fix your Dashboard settings head over to https://branch.app.link/link-settings-page");
+                NSLog(@"If you see a null value on the client side, please temporarily add the following key-value pair to your plist: \n\t<key>AppIdentifierPrefix</key><string>$(AppIdentifierPrefix)</string>\n-> then re-run this test.");
+                NSLog(@"-------------------------------------------------");
+            }
+
+            NSLog(@"-------------------------------------------------------------------------------------------------------------------");
+            NSLog(@"-----To test your deeplink routing append ?validate=true to any branch link and click it on your mobile device-----");
+            NSLog(@"-------------------------------------------------------------------------------------------------------------------");
+            
+            //Popping an alert
+            dispatch_async(dispatch_get_main_queue(), ^{
+                UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Branch integration verification" message:nil preferredStyle:UIAlertControllerStyleAlert];
+                alertController.message = @"Please check your xCode logs for details";
+                // Ok
+                [alertController addAction:[UIAlertAction actionWithTitle:@"Got it" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                }]];
+                [[UIViewController bnc_currentViewController] presentViewController:alertController animated:YES completion:nil];
+            });
+        }
+    }];
 }
 
 - (void)returnToBrowserBasedOnReferringLink:(NSString *)referringLink currentTest:(NSString *)currentTest newTestVal:(NSString *)val {
@@ -103,33 +124,9 @@ void BNCForceBranchValidatorCategoryToLoad(void) {
                 alertController.message = @"Bummer. It seems like +clicked_branch_link is false - we didn't deep link.  Double check that the link you're clicking has the same branch_key that is being used in your .plist file. Return to Safari when you're ready to test again.";
                 [alertController addAction:[UIAlertAction actionWithTitle:@"Got it" style:UIAlertActionStyleDefault handler:nil]];
             }
-            
-            [[self topViewController] presentViewController:alertController animated:YES completion:nil];
+            [[UIViewController bnc_currentViewController] presentViewController:alertController animated:YES completion:nil];
         });
-        
     });
-}
-
-- (UIViewController*)topViewController {
-    return [self topViewControllerWithRootViewController:[UIApplication sharedApplication].keyWindow.rootViewController];
-}
-
-- (UIViewController*)topViewControllerWithRootViewController:(UIViewController*)rootViewController {
-    if ([rootViewController isKindOfClass:[UITabBarController class]]) {
-        UITabBarController* tabBarController = (UITabBarController*)rootViewController;
-        return [self topViewControllerWithRootViewController:tabBarController.selectedViewController];
-    }
-    else if ([rootViewController isKindOfClass:[UINavigationController class]]) {
-        UINavigationController* navigationController = (UINavigationController*)rootViewController;
-        return [self topViewControllerWithRootViewController:navigationController.visibleViewController];
-    }
-    else if (rootViewController.presentedViewController) {
-        UIViewController* presentedViewController = rootViewController.presentedViewController;
-        return [self topViewControllerWithRootViewController:presentedViewController];
-    }
-    else {
-        return rootViewController;
-    }
 }
 
 
