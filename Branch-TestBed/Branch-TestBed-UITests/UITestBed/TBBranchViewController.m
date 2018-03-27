0cfb7bbc5bc44e1b1260c7754580eb70d6c97863
Edward Smith <E-B-Smith@users.noreply.github.com>
2018-03-27 11:57:27 -0700

    Skype Sharing DEVEX-278 (#811)
    * Added Skype as an URL app scraper.
    * Fixed a bug where long URLs with parameters with spaces in them encoded wrong and failed to make a URL.
    * Updated the UITestBed app to test the changes.
    * Updated current window function.
    * Fixed the modal link view window so it doesn't interfere with the activity view controller.

--- Branch-TestBed/Branch-TestBed-UITests/UITestBed/TBBranchViewController.m
+++ Branch-TestBed/Branch-TestBed-UITests/UITestBed/TBBranchViewController.m
@@ -17,11 +17,14 @@
 #import "BNCApplication.h"
 #import "BNCKeyChain.h"
 
-NSString *cononicalIdentifier = @"item/12345";
+NSString *canonicalIdentifier = @"item/12345";
 NSString *canonicalUrl = @"https://dev.branch.io/getting-started/deep-link-routing/guide/ios/";
 NSString *contentTitle = @"Content Title";
 NSString *contentDescription = @"My Content Description";
-NSString *imageUrl = @"https://pbs.twimg.com/profile_images/658759610220703744/IO1HUADP.png";
+NSString *imageUrl =
+    @"http://a57.foxnews.com/images.foxnews.com/content/fox-news/science/2018/03/20/"
+     "first-day-spring-arrives-5-things-to-know-about-vernal-equinox/_jcr_content/"
+     "par/featured_image/media-0.img.jpg/1862/1048/1521552912093.jpg?ve=1&tl=1";
 NSString *feature = @"Sharing Feature";
 NSString *channel = @"Distribution Channel";
 NSString *desktop_url = @"http://branch.io";
@@ -54,6 +57,8 @@ static NSString* TBStringFromObject(id<NSObject> object) {
 @property (nonatomic, strong)   TBTableRow *rewardsRow;
 @end
 
+#pragma mark - TBBranchViewController
+
 @implementation TBBranchViewController
 
 - (void)initializeTableData {
@@ -82,9 +87,9 @@ static NSString* TBStringFromObject(id<NSObject> object) {
     row(@"Send Custom Event", sendCustomEvent:);
 
     section(@"Sharing");
-    row(@"ShareLink from table row", sharelinkTableRow:);
-    row(@"ShareLink no anchor (one day link)", sharelinkTableRowNilAnchor:);
-    row(@"BUO Share from table row", buoShareTableRow:);
+    row(@"ShareLink From Table Row", sharelinkTableRow:);
+    row(@"ShareLink (No Anchor, One Day Link)", sharelinkTableRowNilAnchor:);
+    row(@"BUO Share From Table Row", buoShareTableRow:);
 
     section(@"Rewards");
     self.rewardsRow = row(@"Refresh Rewards", refreshRewards:);
@@ -109,7 +114,7 @@ static NSString* TBStringFromObject(id<NSObject> object) {
     [self initializeTableData];
 
     _universalObject =
-        [[BranchUniversalObject alloc] initWithCanonicalIdentifier: cononicalIdentifier];
+        [[BranchUniversalObject alloc] initWithCanonicalIdentifier:canonicalIdentifier];
     _universalObject.canonicalUrl = canonicalUrl;
     _universalObject.title = contentTitle;
     _universalObject.contentDescription = contentDescription;
@@ -122,6 +127,12 @@ static NSString* TBStringFromObject(id<NSObject> object) {
             @"This text was embedded as data in a Branch link with the following characteristics:\n\n"
              "canonicalUrl: %@\n  title: %@\n  contentDescription: %@\n  imageUrl: %@\n",
                 canonicalUrl, contentTitle, contentDescription, imageUrl];
+    _linkProperties = [[BranchLinkProperties alloc] init];
+    _linkProperties.feature = feature;
+    _linkProperties.channel = channel;
+    _linkProperties.campaign = @"some campaign";
+    [_linkProperties addControlParam:@"$desktop_url" withValue: desktop_url];
+    [_linkProperties addControlParam:@"$ios_url" withValue: ios_url];
 
     UILabel *versionLabel = [[UILabel alloc] initWithFrame:CGRectZero];
     versionLabel.textAlignment = NSTextAlignmentCenter;
@@ -451,13 +462,21 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 
 #pragma mark - Sharing
 
-- (IBAction) sharelinkTableRow:(id)sender {
-    NSIndexPath *indexPath = [self.tableData indexPathForRow:sender];
-    UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+- (IBAction) sharelinkTableRow:(TBTableRow*)sender {
+    UITableViewCell *cell = [self.tableData cellForTableView:self.tableView tableRow:sender];
+    //[self.linkProperties addControlParam:@"$email_subject" withValue:@"Email Subject"];
+    BranchLinkProperties *lp = //NO ? [[BranchLinkProperties alloc] init] : self.linkProperties;
+        [[BranchLinkProperties alloc] init];
+    lp.feature = @"Sharing Feature";
+    lp.channel = @"Distribution Channel";
+    lp.campaign = @"some campaign";
+    [lp addControlParam:@"$desktop_url" withValue:@"http://branch.io"];
+    //[lp addControlParam:@"$ios_url" withValue:@"https://dev.branch.io/getting-started/sdk-integration-guide/guide/ios/"];
     BranchShareLink *shareLink =
         [[BranchShareLink alloc]
             initWithUniversalObject:self.universalObject
-            linkProperties:self.linkProperties];
+            linkProperties:lp/*self.linkProperties*/];
+    shareLink.shareText = @"ShareLink from table row:\n";
     [shareLink presentActivityViewControllerFromViewController:self anchor:cell];
 }
 
@@ -503,14 +522,26 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
         [[BranchShareLink alloc]
             initWithUniversalObject:buo
             linkProperties:self.linkProperties];
+    shareLink.shareText = @"Share link with no anchor:\n";
     [shareLink presentActivityViewControllerFromViewController:self anchor:nil];
 }
 
-- (IBAction) buoShareTableRow:(id)sender {
-    NSIndexPath *indexPath = [self.tableData indexPathForRow:sender];
-    UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
-    [self.universalObject showShareSheetWithLinkProperties:self.linkProperties
-        andShareText:@"Ha ha"
+- (IBAction) buoShareTableRow:(TBTableRow*)sender {
+    BranchUniversalObject *buo =
+        [[BranchUniversalObject alloc] initWithCanonicalIdentifier:canonicalIdentifier];
+    buo.canonicalUrl = @"https://branch.io/deepviews";
+    buo.imageUrl = imageUrl;
+    buo.title = @"Share Title";
+    buo.contentMetadata.customMetadata = (id) @{@"Key": @"Value"};
+
+    BranchLinkProperties *link = [BranchLinkProperties new];
+    [link addControlParam:@"$desktop_url" withValue:@"https://google.com/"];
+    [link addControlParam:@"$email_subject" withValue:@"Email-Subject"];
+    [link addControlParam:@"timestamp" withValue:[NSDate date].description];
+
+    UITableViewCell *cell = [self.tableData cellForTableView:self.tableView tableRow:sender];
+    [buo showShareSheetWithLinkProperties:link
+        andShareText:@"Share Table Row Universal Object:\n"
         fromViewController:self
         anchor:(id)cell
         completionWithError: ^ (NSString * _Nullable activityType, BOOL completed, NSError * _Nullable activityError) {
@@ -520,7 +551,7 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 
 - (IBAction) buoShareBarButton:(id)sender {
     [self.universalObject showShareSheetWithLinkProperties:self.linkProperties
-        andShareText:@"Ha ha"
+        andShareText:@"ShareLink from bar button:\n"
         fromViewController:self
         anchor:sender
         completionWithError: ^ (NSString * _Nullable activityType, BOOL completed, NSError * _Nullable activityError) {
@@ -584,4 +615,3 @@ didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 }
 
 @end
-
