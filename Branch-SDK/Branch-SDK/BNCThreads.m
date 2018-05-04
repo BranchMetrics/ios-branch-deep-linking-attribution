38a355d409880a83e2e6325840d71f2aa4b74014
Edward Smith <esmith@branch.io>
2018-05-03 18:03:06 -0700

    Merge from Apple-Shared-Source.

---
+++ Branch-SDK/Branch-SDK/BNCThreads.m
@@ -0,0 +1,11 @@
+/**
+ @file          BNCThreads.m
+ @package       Branch-SDK
+ @brief         Utilities for working with threads, queues, and blocks.
+
+ @author        Edward Smith
+ @date          May 2017
+ @copyright     Copyright © 2017 Branch. All rights reserved.
+*/
+
+#import "BNCThreads.h"
