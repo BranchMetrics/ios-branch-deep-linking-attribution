e6c8dd6c5494abfbb1b1151be264ce088b450d37
Ernest Cho <echo@branch.io>
2019-08-29 17:13:49 -0700

    INTENG-7505 remove branch view code that relies on UIWebView

--- Branch-SDK/Branch-SDK/BNCPreferenceHelper.m
+++ Branch-SDK/Branch-SDK/BNCPreferenceHelper.m
@@ -676,19 +676,6 @@ static NSString * const BRANCH_PREFS_KEY_ANALYTICS_MANIFEST = @"bnc_branch_analy
 
 #pragma mark - Count Storage
 
-- (void)updateBranchViewCount:(NSString *)branchViewID {
-    NSInteger currentCount = [self getBranchViewCount:branchViewID] + 1;
-    [self writeObjectToDefaults:[BRANCH_PREFS_KEY_BRANCH_VIEW_USAGE_CNT stringByAppendingString:branchViewID] value:@(currentCount)];
-}
-
-- (NSInteger)getBranchViewCount:(NSString *)branchViewID {
-    NSInteger count = [self readIntegerFromDefaults:[BRANCH_PREFS_KEY_BRANCH_VIEW_USAGE_CNT stringByAppendingString:branchViewID]];
-    if (count == NSNotFound){
-        count = 0;
-    }
-    return count;
-}
-
 - (void)saveBranchAnalyticsData:(NSDictionary *)analyticsData {
     if (_sessionID) {
         if (!_savedAnalyticsData) {
