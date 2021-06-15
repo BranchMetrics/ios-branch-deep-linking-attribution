eec283e6e2eda026bc7e508a4eeff18485f407b3
Jimmy Dee <jimmy.dee@branch.io>
2021-06-14 21:51:48 -0700

    Use octet-stream, logging

--- .github/custom-scripts/upload-asset.js
+++ .github/custom-scripts/upload-asset.js
@@ -4,6 +4,8 @@ async function uploadAsset({ github, assetName, path, contentType, uploadUrl }) 
   const contentLength = fs.statSync(path).size;
   const contents = fs.readFileSync(path);
 
+  console.log(`Uploading asset ${assetName}, content-length ${contentLength}.`)
+
   const { data } = await github.repos.uploadReleaseAsset({
     url: uploadUrl + `?name=${encodeURIComponent(assetName)}`,
     headers: {
