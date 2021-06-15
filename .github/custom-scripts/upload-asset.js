d07f50178982acbb5099e10bb042dd20ce720ae3
Jimmy Dee <jimmy.dee@branch.io>
2021-06-14 22:25:10 -0700

    Corrected parameter name.

--- .github/custom-scripts/upload-asset.js
+++ .github/custom-scripts/upload-asset.js
@@ -3,15 +3,17 @@ const fs = require('fs');
 async function uploadAsset({ github, context, releaseId, assetName, path }) {
   const contents = fs.readFileSync(path);
 
-  console.log(`Uploading asset ${assetName}.`)
+  console.log(`Uploading asset ${assetName}...`);
 
   const { data } = await github.repos.uploadReleaseAsset({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    releaseId,
+    release_id: releaseId,
     name: assetName,
     data: contents,
   });
+
+  console.log('Done ✅.');
 
   return data;
 }
