1262078424da75dc81d75c758176dcba9ace268d
nsingh-branch <nipun.singh@branch.io>
2022-03-22 15:57:57 -0700

    Created BranchQRCode files

---
+++ Branch-SDK/BranchQRCode.h
@@ -0,0 +1,28 @@
+//
+//  BranchQRCode.h
+//  Branch
+//
+//  Created by Nipun Singh on 3/22/22.
+//  Copyright © 2022 Branch, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNCServerInterface.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BranchQRCode : NSObject
+
+//@property (nonatomic, copy, readonly) NSString *crossPlatformID;
+//@property (nonatomic, copy, readonly) NSString *developerID;
+//@property (nonatomic, strong, readonly) NSArray<NSString *> *pastCrossPlatformIDs;
+//@property (nonatomic, strong, readonly) NSArray<BranchProbabilisticCrossPlatformID *> *probabiliticCrossPlatformIDs;
+//
+//+ (nullable BranchCrossPlatformID *)buildFromJSON:(NSDictionary *)json;
+//
+//+ (void)requestCrossPlatformIdData:(BNCServerInterface *)serverInterface key:(NSString *)key completion:(void(^) (BranchCrossPlatformID * _Nullable cpid))completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
---
+++ Branch-SDK/BranchQRCode.m
@@ -0,0 +1,8 @@
+//
+//  BranchQRCode.m
+//  Branch
+//
+//  Created by Nipun Singh on 3/22/22.
+//
+
+#import <Foundation/Foundation.h>
