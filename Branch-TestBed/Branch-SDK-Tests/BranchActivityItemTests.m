e89c4c8b526b85993b9e8ac90f52537ae3a9bbc0
nsingh-branch <nipun.singh@branch.io>
2023-10-11 14:34:33 -0700

    Update BranchActivityItemTests.m

--- Branch-TestBed/Branch-SDK-Tests/BranchActivityItemTests.m
+++ Branch-TestBed/Branch-SDK-Tests/BranchActivityItemTests.m
@@ -14,29 +14,22 @@
 
 @implementation BranchActivityItemTests
 
-- (void)setUp {
-    [super setUp];
-}
-
-- (void)tearDown {
-    [super tearDown];
-}
-
 - (void)testGetBranchActivityItemWithAllParams {
     NSDictionary *params = @{@"key": @"value"};
     NSString *feature = @"feature4";
     NSString *stage = @"stage3";
     NSArray *tags = @[@"tag3", @"tag4"];
     NSString *campaign = @"campaign1";
-    NSString *alias = @"alias1";
+    NSString *alias = [[NSUUID UUID] UUIDString];
     BranchActivityItemProvider *provider = [Branch getBranchActivityItemWithParams:params feature:feature stage:stage campaign:campaign tags:tags alias:alias];
+    sleep(2000);
     if ([[provider item] isKindOfClass:[NSURL class]]) {
         NSURL *urlObject = (NSURL *)[provider item];
         NSString *url = [urlObject absoluteString];
         
         NSLog(@"Provider URL as String: %@", url);
         
-        XCTAssertTrue([url isEqualToString:@"https://bnctestbed.app.link/alias1"]);
+        XCTAssertTrue([url isEqualToString:[@"https://bnctestbed.app.link/" stringByAppendingString:alias]]);
     } else {
         XCTFail("Provider Data is not of type NSURL");
     }
