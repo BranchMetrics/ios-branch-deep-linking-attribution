2009c84ae1b603d963a5e064c2f5eb7e97f6f242
echo <echo@branch.io>
2024-04-02 14:30:36 -0700

    Allow init calls to come in late.

--- Sources/BranchSDK/Branch.m
+++ Sources/BranchSDK/Branch.m
@@ -625,27 +625,20 @@ static NSString *bnc_branchKey = nil;
     [self.class addBranchSDKVersionToCrashlyticsReport];
     self.shouldAutomaticallyDeepLink = automaticallyDisplayController;
 
-    // If the SDK is already initialized, this means that initSession was called after other lifecycle calls.
-    if (self.initializationStatus == BNCInitStatusInitialized) {
-        [self initUserSessionAndCallCallback:YES sceneIdentifier:nil urlString:nil];
-        return;
-    }
-
-    // Save data from push notification on app launch
+    // Check for Branch link in a push payload
+    NSString *pushURL = nil;
     #if !TARGET_OS_TV
     if ([options objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey]) {
         id branchUrlFromPush = [options objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey][BRANCH_PUSH_NOTIFICATION_PAYLOAD_KEY];
         if ([branchUrlFromPush isKindOfClass:[NSString class]]) {
             self.preferenceHelper.universalLinkUrl = branchUrlFromPush;
             self.preferenceHelper.referringURL = branchUrlFromPush;
+            pushURL = (NSString *)branchUrlFromPush;
         }
     }
     #endif
 
-    // Handle case where there's no URI scheme or Universal Link.
-    if (![options.allKeys containsObject:UIApplicationLaunchOptionsURLKey] && ![options.allKeys containsObject:UIApplicationLaunchOptionsUserActivityDictionaryKey]) {
-        [self initUserSessionAndCallCallback:YES sceneIdentifier:nil urlString:nil];
-    }
+    [self initUserSessionAndCallCallback:YES sceneIdentifier:nil urlString:pushURL];
 }
 
 - (void)setDeepLinkDebugMode:(NSDictionary *)debugParams {
