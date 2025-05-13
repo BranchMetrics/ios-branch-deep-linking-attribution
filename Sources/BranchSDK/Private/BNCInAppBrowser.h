182e09f7e33ff43d19385ff37632d381e7b6aacf
NidhiDixit09 <93544270+NidhiDixit09@users.noreply.github.com>
2025-05-13 12:12:30 -0700

    Added tvOS Macros

--- Sources/BranchSDK/Private/BNCInAppBrowser.h
+++ Sources/BranchSDK/Private/BNCInAppBrowser.h
@@ -6,15 +6,27 @@
 //  Copyright © 2025 Branch, Inc. All rights reserved.
 //
 
+#if !TARGET_OS_TV
 #import <Foundation/Foundation.h>
 #import <SafariServices/SafariServices.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface BNCInAppBrowser : NSObject <SFSafariViewControllerDelegate>
+/**
+ Returns the shared singleton instance of `BNCInAppBrowser`.
+ @return A shared instance of `BNCInAppBrowser`, or `nil` if `SFSafariViewController` is not available.
+ */
 + (instancetype)sharedInstance;
+
+/**
+ Opens the given URL in a `SFSafariViewController`over  current top-most view controller.
+ @param  urlStr The URL string to be opened.
+ */
 - (void)openURLInSafariVC:(NSString *) urlStr;
+
 @end
 
 NS_ASSUME_NONNULL_END
 
+#endif
