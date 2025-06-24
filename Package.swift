dc2d9c7319811f9dfa22d7859bd494d4020acfe0
NidhiDixit09 <93544270+NidhiDixit09@users.noreply.github.com>
2025-06-24 15:48:32 -0700

    Added target for swift code.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "BranchSDK",
+    name: "ios-branch-deep-linking-attribution",
     platforms: [
         .iOS(.v12),
         .tvOS(.v12),
@@ -18,16 +18,17 @@ let package = Package(
     targets: [
         .target(
             name: "BranchSDK",
-            path: "Sources",
-            sources: [
-                "BranchSDK/"
+            dependencies: [
+                "BranchSDK_Swift"
             ],
+            path: "Sources/BranchSDK",
             resources: [
-                .copy("Resources/PrivacyInfo.xcprivacy"),
+                .copy("../Resources/PrivacyInfo.xcprivacy"),
             ],
-            publicHeadersPath: "BranchSDK/Public/",
+            publicHeadersPath: "Public",
             cSettings: [
-                .headerSearchPath("BranchSDK/Private"),
+                .headerSearchPath("."),
+                .headerSearchPath("Private")
             ],
             linkerSettings: [
                 .linkedFramework("CoreServices"),
@@ -36,6 +37,10 @@ let package = Package(
                 .linkedFramework("CoreSpotlight", .when(platforms: [.iOS])),
                 .linkedFramework("AdServices", .when(platforms: [.iOS]))
             ]
+        ),
+        .target(
+            name: "BranchSDK_Swift",
+            path: "Sources/BranchSDK_Swift",
         ),
     ]
 )
