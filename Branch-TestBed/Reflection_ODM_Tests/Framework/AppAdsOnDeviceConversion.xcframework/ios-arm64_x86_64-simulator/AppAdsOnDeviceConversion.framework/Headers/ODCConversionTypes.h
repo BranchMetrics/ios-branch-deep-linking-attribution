6220b105b3f35942e07c454732e9dda1af506ca4
NidhiDixit09 <93544270+NidhiDixit09@users.noreply.github.com>
2025-08-27 10:56:20 -0700

    Replaced ODM framework binary with SPM

--- Branch-TestBed/Reflection_ODM_Tests/Framework/AppAdsOnDeviceConversion.xcframework/ios-arm64_x86_64-simulator/AppAdsOnDeviceConversion.framework/Headers/ODCConversionTypes.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-
-/// The type of interaction for fetching conversion info.
-typedef NS_ENUM(NSInteger, ODCInteractionType) {
-  ODCInteractionTypeInstallation,
-} NS_SWIFT_NAME(InteractionType);
