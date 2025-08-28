1b85a2b7e90ca242002e9dd9e49734cfde7b14fd
NidhiDixit09 <93544270+NidhiDixit09@users.noreply.github.com>
2025-08-28 01:34:37 -0700

    Added  code in test app.

--- SDKIntegrationTestApps/iOSReleaseTest-SPM/iOSReleaseTest/AppDelegate.swift
+++ SDKIntegrationTestApps/iOSReleaseTest-SPM/iOSReleaseTest/AppDelegate.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import BranchSDK
+import GoogleUtilities
+import GoogleAdsOnDeviceConversion
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,8 +17,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GULSetLoggerLevel(.max)
         // Override point for customization after application launch.
-        
+     //   Branch.getInstance().enableLogging()
+       // Set the time when the app was first launched.
+        ConversionManager.sharedInstance.setFirstLaunchTime(Date())
+
+        // Fetch the conversion info.
+        ConversionManager.sharedInstance.fetchAggregateConversionInfo(for: .installation)
+        { aggregateConversionInfo, error in
+            print("Conversion info Error  ************** \(error)")
+           guard error == nil else { return }
+           guard let info = aggregateConversionInfo else {
+              // Troubleshoot:
+              // 1. Check that the Date passed to setFirstLaunchTime() was when the app
+              //    first launched.
+              // 2. Check that your app is running in an approved region.
+              return
+           }
+           guard info.count > 0 else { return }
+
+           print("Conversion info ************** \(info)")
+           // Use info as the value in the odm_info query parameter in
+           // the App Conversion API detailed in Step 6.
+           // For example, if info is "abcdEfadGdaf", then odm_info=abcdEfadGdaf.
+        }
         
         return true
     }
