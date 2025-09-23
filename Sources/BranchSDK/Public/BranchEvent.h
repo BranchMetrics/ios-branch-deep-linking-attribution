aca755bad40e7ba4ed2edcf7e5f90f7529aaa994
NidhiDixit09 <93544270+NidhiDixit09@users.noreply.github.com>
2025-09-23 11:01:17 -0700

    Disabled Deprecation warning for StoreKit(Original) APIs

--- Sources/BranchSDK/Public/BranchEvent.h
+++ Sources/BranchSDK/Public/BranchEvent.h
@@ -60,6 +60,9 @@ typedef NS_ENUM(NSInteger, BranchEventAdType) {
 
 #pragma mark - BranchEvent
 
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 @interface BranchEvent : NSObject <SKProductsRequestDelegate>
 
 - (instancetype _Nonnull) initWithName:(NSString*_Nonnull)name NS_DESIGNATED_INITIALIZER;
@@ -117,6 +120,8 @@ typedef NS_ENUM(NSInteger, BranchEventAdType) {
 - (void) logEventWithTransaction:(SKPaymentTransaction*_Nonnull)transaction;
 
 @end
+
+#pragma clang diagnostic pop
 
 #pragma mark - BranchEventRequest
 
