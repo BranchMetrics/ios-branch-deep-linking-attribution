c8ef2101d49e641b816323f35fc7653dae8fdd08
Willian Pinho <willianjunior.peresdepinho.ext@branch.io>
2025-12-16 15:13:28 -0300

    Fix bridging header paths for CocoaPods compatibility
    Remove Public/ and Private/ path prefixes from import statements
    in Branch-Bridging-Header.h. CocoaPods flattens all headers into
    a single directory, so relative paths don't work in the built
    framework.

--- Sources/BranchSDK/Branch-Bridging-Header.h
+++ Sources/BranchSDK/Branch-Bridging-Header.h
@@ -5,10 +5,10 @@
 //  Bridging header to expose Objective-C types to Swift within the Branch target
 //
 
-#import "Public/Branch.h"
-#import "Public/BNCServerRequest.h"
-#import "Public/BNCServerInterface.h"
-#import "Public/BNCServerResponse.h"
-#import "Public/BNCPreferenceHelper.h"
-#import "Public/BranchLogger.h"
-#import "Private/NSError+Branch.h"
+#import "Branch.h"
+#import "BNCServerRequest.h"
+#import "BNCServerInterface.h"
+#import "BNCServerResponse.h"
+#import "BNCPreferenceHelper.h"
+#import "BranchLogger.h"
+#import "NSError+Branch.h"
