2a0109c89af3a99567e3a4ad1dbb470192d5f369
Willian Pinho <willianjunior.peresdepinho.ext@branch.io>
2026-02-18 17:00:15 -0300

    chore: resolve merge conflicts with 4.0.0-alpha.0 base branch
    - Resolve 8 conflicting files favoring ObjC-only approach
    - Accept base branch BranchConfigurationController (BB-prefixed refs)
    - Remove duplicate B70D BranchConfigurationController refs from HEAD
    - Keep BNCServerRequestOperation refs from NSOperationQueue migration
    - Update BranchSDKTests to use ObjC BranchConfigurationController
    - Accept base branch podspec changes

--- BranchSDKTests/BranchConfigurationControllerTests.m
+++ BranchSDKTests/BranchConfigurationControllerTests.m
@@ -11,11 +11,7 @@
 #import "BNCRequestFactory.h"
 #import "BNCEncodingUtils.h"
 
-#if SWIFT_PACKAGE
-@import BranchSwiftSDK;
-#else
-#import "Private/BranchConfigurationController.h"
-#endif
+#import "BranchConfigurationController.h"
 
 @interface BranchConfigurationControllerTests : XCTestCase
 @end
@@ -23,37 +19,25 @@
 @implementation BranchConfigurationControllerTests
 
 - (void)testSingletonInstance {
-#if SWIFT_PACKAGE
-    ConfigurationController *instance1 = [ConfigurationController shared];
-    XCTAssertNotNil(instance1);
-
-    ConfigurationController *instance2 = [ConfigurationController shared];
-    XCTAssertEqual(instance1, instance2);
-#else
     BranchConfigurationController *instance1 = [BranchConfigurationController sharedInstance];
     XCTAssertNotNil(instance1);
 
     BranchConfigurationController *instance2 = [BranchConfigurationController sharedInstance];
     XCTAssertEqual(instance1, instance2);
-#endif
 }
 
 - (void)testPropertySettersAndGetters {
-#if SWIFT_PACKAGE
-    ConfigurationController *configController = [ConfigurationController shared];
-#else
     BranchConfigurationController *configController = [BranchConfigurationController sharedInstance];
-#endif
-    
+
     NSString *keySource = BRANCH_KEY_SOURCE_GET_INSTANCE_API;
     configController.branchKeySource = keySource;
     XCTAssertTrue([configController.branchKeySource isEqualToString:keySource]);
-    
+
     configController.deferInitForPluginRuntime = YES;
     XCTAssertTrue(configController.deferInitForPluginRuntime);
     configController.deferInitForPluginRuntime = NO;
     XCTAssertFalse(configController.deferInitForPluginRuntime);
-    
+
     configController.checkPasteboardOnInstall = YES;
     XCTAssertTrue(configController.checkPasteboardOnInstall);
     configController.checkPasteboardOnInstall = NO;
@@ -61,11 +45,7 @@
 }
 
 - (void)testGetConfiguration {
-#if SWIFT_PACKAGE
-    ConfigurationController *configController = [ConfigurationController shared];
-#else
     BranchConfigurationController *configController = [BranchConfigurationController sharedInstance];
-#endif
     configController.branchKeySource = BRANCH_KEY_SOURCE_INFO_PLIST;
     configController.deferInitForPluginRuntime = YES;
     configController.checkPasteboardOnInstall = YES;
@@ -76,10 +56,10 @@
     XCTAssertTrue([configDict[BRANCH_REQUEST_KEY_BRANCH_KEY_SOURCE] isEqualToString:BRANCH_KEY_SOURCE_INFO_PLIST]);
     XCTAssertEqualObjects(configDict[BRANCH_REQUEST_KEY_DEFER_INIT_FOR_PLUGIN_RUNTIME], @(YES));
     XCTAssertEqualObjects(configDict[BRANCH_REQUEST_KEY_CHECK_PASTEBOARD_ON_INSTALL], @(YES));
-    
+
     NSDictionary *frameworks = configDict[BRANCH_REQUEST_KEY_LINKED_FRAMEORKS];
     XCTAssertNotNil(frameworks);
-    
+
     // These checks verify that framework detection returns a boolean value.
     // The actual value depends on the runtime environment (simulator/device).
     XCTAssertNotNil(frameworks[FRAMEWORK_AD_SUPPORT]);
@@ -91,11 +71,7 @@
 }
 
 - (void)testInstallRequestParams {
-#if SWIFT_PACKAGE
-    ConfigurationController *configController = [ConfigurationController shared];
-#else
     BranchConfigurationController *configController = [BranchConfigurationController sharedInstance];
-#endif
     configController.branchKeySource = BRANCH_KEY_SOURCE_INFO_PLIST;
     configController.deferInitForPluginRuntime = YES;
     configController.checkPasteboardOnInstall = YES;
@@ -104,17 +80,17 @@
     NSNumber* requestCreationTimeStamp = BNCWireFormatFromDate([NSDate date]);
     BNCRequestFactory *factory = [[BNCRequestFactory alloc] initWithBranchKey:@"key_abcd" UUID:requestUUID TimeStamp:requestCreationTimeStamp];
     NSDictionary *installDict = [factory dataForInstallWithURLString:@"https://branch.io"];
-    
+
     NSDictionary *configDict = installDict[BRANCH_REQUEST_KEY_OPERATIONAL_METRICS];
     XCTAssertNotNil(configDict);
 
     XCTAssertTrue([configDict[BRANCH_REQUEST_KEY_BRANCH_KEY_SOURCE] isEqualToString:BRANCH_KEY_SOURCE_INFO_PLIST]);
     XCTAssertEqualObjects(configDict[BRANCH_REQUEST_KEY_DEFER_INIT_FOR_PLUGIN_RUNTIME], @(YES));
     XCTAssertEqualObjects(configDict[BRANCH_REQUEST_KEY_CHECK_PASTEBOARD_ON_INSTALL], @(YES));
-    
+
     NSDictionary *frameworks = configDict[BRANCH_REQUEST_KEY_LINKED_FRAMEORKS];
     XCTAssertNotNil(frameworks);
-    
+
     // These checks verify that framework detection returns a boolean value.
     // The actual value depends on the runtime environment (simulator/device).
     XCTAssertNotNil(frameworks[FRAMEWORK_AD_SUPPORT]);
